b2c53217957c46f2d44a9b37aeceedbae4225b54
Michael Koval <koval.michael@gmail.com>
2010-01-03 02:48:07 -0500

    More cleanup and starting to compile the code to find missing extern and #defines.

--- hax_pic.c
+++ hax_pic.c
@@ -8,58 +8,56 @@ uint16_t kSlowSpeed = 18500;
  * HARDWARE SPECIFIC DEFINITIONS
  */
 #define RND 6
-
-#define DATA_SIZE        30
-#define SPI_TRAILER_SIZE 2
-#define SPI_XFER_SIZE    (DATA_SIZE + SPI_TRAILER_SIZE)
-
-#define RESET_VECTOR    0x800
-#define HIGH_INT_VECTOR 0x808
-#define LOW_INT_VECTOR  0x818
+#define RESET_VECTOR     0x800
 
 /* This structure contains important system status information. */
 typedef struct {
-  unsigned int :6;
-  unsigned int autonomous:1; /* Autonomous enable/disable flag. */
-  unsigned int disabled:1;   /* Robot enable/disable flag. */
-} modebits;
+	uint8_t:6;
+	uint8_t autonomous:1; /* Autonomous enable/disable flag. */
+	uint8_t disabled:1;   /* Robot enable/disable flag. */
+} PICModes;
 
-/* This structure allows you to address specific bits of a byte.*/
+/* This structure allows you to address specific bits of a byte. Changed to
+ * an eight-bit integer from IFI's definition avoid reliance upon the MCC18
+ * restriction of bitfields to not exceed a byte in length.
+ */
 typedef struct {
-	unsigned int bit0:1;
-	unsigned int bit1:1;
-	unsigned int bit2:1;
-	unsigned int bit3:1;
-	unsigned int bit4:1;
-	unsigned int bit5:1;
-	unsigned int bit6:1;
-	unsigned int bit7:1;
-} bitid;
+	uint8_t bit0:1;
+	uint8_t bit1:1;
+	uint8_t bit2:1;
+	uint8_t bit3:1;
+	uint8_t bit4:1;
+	uint8_t bit5:1;
+	uint8_t bit6:1;
+	uint8_t bit7:1;
+} Bits;
 
-/* This structure defines the contents of the data received from the Master
- * microprocessor. */
+/* This structure defines the contents of the data received from the master
+ * processor.
+ */
 typedef struct {
-	unsigned char packet_num;
+	uint8_t packet_num;
 	
 	union {
-		bitid bitselect;
-		modebits mode;
-		unsigned char allbits;
+		Bits bitselect;
+		PICModes mode;
+		uint8_t allbits;
 	} rc_mode_byte;
 	
 	union {
-		bitid bitselect;
-		unsigned char allbits;
+		Bits bitselect;
+		uint8_t allbits;
 	} rc_receiver_status_byte;
 	
-	unsigned char spare01,spare02,spare03; /* Reserved for future use. */
+	/* Reserved for future use. */
+	uint8_t spare01, spare02, spare03;
 
-	unsigned char oi_analog01, oi_analog02, oi_analog03, oi_analog04;
-	unsigned char oi_analog05, oi_analog06, oi_analog07, oi_analog08;
-	unsigned char oi_analog09, oi_analog10, oi_analog11, oi_analog12;
-	unsigned char oi_analog13, oi_analog14, oi_analog15, oi_analog16;
-	unsigned char reserve[9]; /* Reserved for future use. */
-	unsigned char master_version;
+	uint8_t oi_analog01, oi_analog02, oi_analog03, oi_analog04;
+	uint8_t oi_analog05, oi_analog06, oi_analog07, oi_analog08;
+	uint8_t oi_analog09, oi_analog10, oi_analog11, oi_analog12;
+	uint8_t oi_analog13, oi_analog14, oi_analog15, oi_analog16;
+	uint8_t reserve[9]; /* Reserved for future use. */
+	uint8_t master_version;
 } RxData;
 
 /* This structure defines the contents of the data transmitted to the master  
@@ -67,32 +65,32 @@ typedef struct {
  */
 typedef struct {
 	/* Reserved for future use. */
-	unsigned char spare01,spare02,spare03,spare04;
+	uint8_t spare01, spare02, spare03, spare04;
 	
-	unsigned char rc_pwm01, rc_pwm02, rc_pwm03, rc_pwm04;
-	unsigned char rc_pwm05, rc_pwm06, rc_pwm07, rc_pwm08;
-	unsigned char rc_pwm09, rc_pwm10, rc_pwm11, rc_pwm12;
-	unsigned char rc_pwm13, rc_pwm14, rc_pwm15, rc_pwm16;
+	uint8_t rc_pwm01, rc_pwm02, rc_pwm03, rc_pwm04;
+	uint8_t rc_pwm05, rc_pwm06, rc_pwm07, rc_pwm08;
+	uint8_t rc_pwm09, rc_pwm10, rc_pwm11, rc_pwm12;
+	uint8_t rc_pwm13, rc_pwm14, rc_pwm15, rc_pwm16;
 	
-	unsigned char user_cmd;   /* Reserved for future use. */
-	unsigned char cmd_byte1;  /* Reserved for future use. */
-	unsigned char pwm_mask;
-	unsigned char warning_code;
-	unsigned char reserve[4]; /* Reserved for future use. */
-	unsigned char error_code;
-	unsigned char packetnum;
-	unsigned char current_mode;
-	unsigned char control;
+	uint8_t user_cmd;   /* Reserved for future use. */
+	uint8_t cmd_byte1;  /* Reserved for future use. */
+	uint8_t pwm_mask;
+	uint8_t warning_code;
+	uint8_t reserve[4]; /* Reserved for future use. */
+	uint8_t error_code;
+	uint8_t packetnum;
+	uint8_t current_mode;
+	uint8_t control;
 } TxData;
 
 typedef struct {
-	unsigned int NEW_SPI_DATA:1;
-	unsigned int TX_UPDATED:1;
-	unsigned int FIRST_TIME:1;
-	unsigned int TX_BUFFSELECT:1;
-	unsigned int RX_BUFFSELECT:1;
-	unsigned int SPI_SEMAPHORE:1;
-	unsigned int:2;
+	uint8_t NEW_SPI_DATA:1;
+	uint8_t TX_UPDATED:1;
+	uint8_t FIRST_TIME:1;
+	uint8_t TX_BUFFSELECT:1;
+	uint8_t RX_BUFFSELECT:1;
+	uint8_t SPI_SEMAPHORE:1;
+	uint8_t:2;
 } Packed;
 
 extern volatile near unsigned long short TBLPTR;
@@ -112,7 +110,7 @@ extern void main (void);
 
 void _entry(void);
 void _startup(void);
-void _do_cinit(void);
+void _do_cinit(void);I
 
 #pragma code _entry_scn=RESET_VECTOR
 void _entry(void) {
@@ -182,8 +180,9 @@ void _do_cinit(void) {
 		tstfsz curr_entry, 1
 		bra 1
 		_endasm
-		
+	
 	goto done;
+	
 	/* Count down so we only have to look up the data in _cinit once. At
 	 * this point we know that TBLPTR points to the top of the current entry
 	 * in _cinit, so we can just start reading the from, to, and size
