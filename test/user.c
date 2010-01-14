1f10322564e46bc2a719817cda2dee170bb773d2
John Mesmon <jmesmon@gmail.com>
2010-01-14 00:21:37 -0500

    Preliminary Test code.

--- test/user.c
+++ test/user.c
@@ -3,17 +3,26 @@
 uint8_t kNumAnalogInputs = 2;
 
 void init(void) {
+	putc('I');
 }
 
 void auton_loop(void) {
+	putc('A');
+	putc('l');
 }
 	
 void auton_spin(void) {
+	putc('A');
+	putc('s');
 }
 
 void telop_loop(void) {
+	putc('T');
+	putc('l');
 }
 
 void telop_spin(void) {
+	putc('T');
+	putc('s');
 }
 
