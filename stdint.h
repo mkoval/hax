045f2bafc3253601535bd0f3d2f8c72e44629f00
Michael Koval <koval.michael@gmail.com>
2010-01-27 23:18:30 -0500

    specifically indicate signed stuff

--- stdint.h
+++ stdint.h
@@ -3,13 +3,13 @@
 
 /* As defined in Section 2.1, Page 11 of the MCC18 C18 User Guide */
 #ifdef __18CXX
-typedef char int8_t;
+typedef signed   char int8_t;
 typedef unsigned char uint8_t;
 
-typedef int int16_t;
+typedef signed   int int16_t;
 typedef unsigned int uint16_t;
 
-typedef long int32_t;
+typedef signed   long int32_t;
 typedef unsigned long uint32_t;
 #endif
 
