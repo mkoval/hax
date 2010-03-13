674042ec66e68a6c85fe0d4d83e621f0bc643c95
Michael Koval <koval.michael@gmail.com>
2010-03-13 15:02:27 -0500

    Stopped the generation of .d dependancy files.

--- cortex/gcc.mk
+++ cortex/gcc.mk
@@ -1,14 +1,10 @@
-
 CC           = 'arm-none-eabi-gcc'
 LD           = 'arm-none-eabi-gcc'
 AS           = 'arm-none-eabi-gcc'
 CCOUT_OPT    = -o 
-ARCH_CFLAGS  = -MD -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb              \
-               -mcpu=cortex-m3 -fsigned-char -ffunction-sections -Wall         \
-               -mlittle-endian -c
-
+ARCH_CFLAGS  = -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb -mcpu=cortex-m3 \
+               -fsigned-char -ffunction-sections -Wall -mlittle-endian -c
 ARCH_AFLAGS  =
 ARCH_LDFLAGS = -mcpu=cortex-m3 -mthumb -Wl,-T -Xlinker -u _start -Wl,-static   \
                -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker
 TRASH        = $(CSOURCE:.c=.c.d)
-
