d5ca76857d5e5f722fc54e22c7fbb36040a4105a
Michael Koval <koval.michael@gmail.com>
2010-03-13 03:39:44 -0500

    Added a GCC section to compilers.h. ROM and overlay are currently defined as the empty string, since they seem to be unsupported.

--- compilers.h
+++ compilers.h
@@ -11,6 +11,13 @@
 #define __rom	rom
 #define __overlay overlay
 
+/* GCC 4.3.2 does not seem to support ROM or overlay attributes. */
+#elif defined(__ARM_EABI__) && defined(__GNUC__)
+
+#define __noreturn __attribute__((__noreturn__))
+#define __rom
+#define __overlay
+
 #else
 
 #error "Unsupported Compiler"
