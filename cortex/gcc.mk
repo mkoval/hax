1cada17dc9ee48a39feeaea5b394b64288b9908b
Michael Koval <koval.michael@gmail.com>
2010-03-13 04:12:25 -0500

    Removed unnecessary include paths from the cortex, gcc-specific makefile.

--- cortex/gcc.mk
+++ cortex/gcc.mk
@@ -1,18 +1,14 @@
 
-# TODO Figure out why './' must be explicit to include top-level headers.
-
 CC           = 'arm-none-eabi-gcc'
 LD           = 'arm-none-eabi-gcc'
 AS           = 'arm-none-eabi-gcc'
-IPATH        = '/usr/local/stm32/include/'
-LIBPATH      = '/usr/local/stm32/lib/'
 CCOUT_OPT    = -o 
 ARCH_CFLAGS  = -MD -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb              \
                -mcpu=cortex-m3 -fsigned-char -ffunction-sections -Wall         \
-               -mlittle-endian -c -I$(IPATH)
+               -mlittle-endian -c
 
 ARCH_AFLAGS  =
 ARCH_LDFLAGS = -mcpu=cortex-m3 -mthumb -Wl,-T -Xlinker -u _start -Wl,-static   \
-               -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker -L$(LIBPATH)
+               -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker
 TRASH        = $(CSOURCE:.c=.c.d)
 
