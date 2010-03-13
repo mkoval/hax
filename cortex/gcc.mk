1a170c5b10ff9964870048fe196e9909fa15ab37
Michael Koval <koval.michael@gmail.com>
2010-03-13 04:01:53 -0500

    Removed the -I'./' flag to avoid overriding the compiler-provided stdint.h with our custom one (maybe?). Still need to figure this one out...

--- cortex/gcc.mk
+++ cortex/gcc.mk
@@ -9,11 +9,10 @@ LIBPATH      = '/usr/local/stm32/lib/'
 CCOUT_OPT    = -o 
 ARCH_CFLAGS  = -MD -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb              \
                -mcpu=cortex-m3 -fsigned-char -ffunction-sections -Wall         \
-               -mlittle-endian -c -I$(IPATH) -I'./'
+               -mlittle-endian -c -I$(IPATH)
 
 ARCH_AFLAGS  =
 ARCH_LDFLAGS = -mcpu=cortex-m3 -mthumb -Wl,-T -Xlinker -u _start -Wl,-static   \
-               -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker -L$(LIBPATH)  \
-               -I'./'
+               -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker -L$(LIBPATH)
 TRASH        = $(CSOURCE:.c=.c.d)
 
