684d03b866a00cd6358d200a845e732dd008f092
Michael Koval <koval.michael@gmail.com>
2010-03-16 02:00:03 -0400

    Added the SIGN() helper-macro to greatly simplify the code in robot.c.

--- nitish/util.h
+++ nitish/util.h
@@ -4,6 +4,7 @@
 #define ABS(_x_)      (((_x_) > 0) ? (_x_) : -(_x_))
 #define MIN(_x_, _y_) (((_x_) < (_y_)) ? (_x_) : (_y_))
 #define MAX(_x_, _y_) (((_x_) > (_y_)) ? (_x_) : (_y_))
+#define SIGN(_x_)     (((_x_) < 0) ? -1 : 1)
 #define PROP(_mout_, _merr_, _err_) ((_mout_)*MIN((_merr_), (_merr_))/(_merr_))
 
 #endif
