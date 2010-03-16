62fd7972466cf98134589f8eb068a0ee71ffd609
Michael Koval <koval.michael@gmail.com>
2010-03-16 18:14:32 -0400

    Added the appropriate flags to ALL_LDFLAGS to link with the EasyC binary. Unfortunately, I'm still missing the .LST linker script that is auto-generated by EasyC (seemingly) prior to linking.

--- cortex/gcc.mk
+++ cortex/gcc.mk
@@ -1,11 +1,14 @@
 CC           = 'arm-none-eabi-gcc'
 LD           = 'arm-none-eabi-gcc'
-AS           = 'arm-none-eabi-gcc'
+AS           = 'arm-none-eabi-as'
 CCOUT_OPT    = -o 
+LDOUT_OPT    = -o 
 ARCH_CFLAGS  = -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb -mcpu=cortex-m3  \
                -fsigned-char -ffunction-sections -Wall -mlittle-endian         \
                -Wno-main -c
 ARCH_AFLAGS  =
-ARCH_LDFLAGS = -mcpu=cortex-m3 -mthumb -Wl,-T -Xlinker -u _start -Wl,-static   \
-               -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker
+ARCH_LDFLAGS = -Wl,-T -Xlinker 'cortex/vexlib/Scripts/easyCRuntimeInit.elf.ld' \
+               -L'cortex/vexlib/Startup' -L'cortex/vexlib/easyCRuntime'        \
+               -L'cortex/vexlib/Link' -mpu=cortex-m3 -mthumb -u _start         \
+               -Wl,-static -Wl,--gc-sections -nostartfiles -Wl,-Map
 TRASH        = $(CSOURCE:.c=.c.d)
