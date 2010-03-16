3616284108163e2401bd34a079bea25826b96d4f
Michael Koval <koval.michael@gmail.com>
2010-03-16 01:39:34 -0400

    Added the PROP() macro for simple plug-and-chug P-loop control.

--- nitish/util.h
+++ nitish/util.h
@@ -4,5 +4,6 @@
 #define ABS(_x_)      (((_x_) > 0) ? (_x_) : -(_x_))
 #define MIN(_x_, _y_) (((_x_) < (_y_)) ? (_x_) : (_y_))
 #define MAX(_x_, _y_) (((_x_) > (_y_)) ? (_x_) : (_y_))
+#define PROP(_mout_, _merr_, _err_) ((_mout_)*MIN((_merr_), (_merr_))/(_merr_))
 
 #endif
