a1ade1c7449eef91064b6861c124fa56b8b84902
Michael Koval <koval.michael@gmail.com>
2010-03-16 17:12:41 -0400

    Disabled the warning about using 'void main()' instead of 'int main()'.

--- cortex/gcc.mk
+++ cortex/gcc.mk
@@ -2,8 +2,9 @@ CC           = 'arm-none-eabi-gcc'
 LD           = 'arm-none-eabi-gcc'
 AS           = 'arm-none-eabi-gcc'
 CCOUT_OPT    = -o 
-ARCH_CFLAGS  = -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb -mcpu=cortex-m3 \
-               -fsigned-char -ffunction-sections -Wall -mlittle-endian -c
+ARCH_CFLAGS  = -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb -mcpu=cortex-m3  \
+               -fsigned-char -ffunction-sections -Wall -mlittle-endian         \
+               -Wno-main -c
 ARCH_AFLAGS  =
 ARCH_LDFLAGS = -mcpu=cortex-m3 -mthumb -Wl,-T -Xlinker -u _start -Wl,-static   \
                -Wl,--gc-sections -nostartfiles -Wl,-Map -Xlinker
