508b791f1e5ec3f962e3f82264ab8179f61b1cc2
Michael Koval <koval.michael@gmail.com>
2010-03-16 01:59:34 -0400

    Added the _raw suffix to functions that are used to directly control the hardware with little intermediate logic. This helps avoid naming conflicts with local variables such as arm or ramp.

--- nitish/robot.c
+++ nitish/robot.c
@@ -6,7 +6,7 @@
 
 #include "robot.h"
 
-void drive(AnalogOut forward, AnalogOut strafe, AnalogOut turn) {
+void drive_raw(AnalogOut forward, AnalogOut strafe, AnalogOut turn) {
 	int16_t left  = (int16_t) forward - turn;
 	int16_t right = (int16_t)-forward - turn;
 	int16_t max   = MAX(left, right);
@@ -23,7 +23,7 @@ void drive(AnalogOut forward, AnalogOut strafe, AnalogOut turn) {
 	motor_set(MTR_STRAFE_F, strafe);
 }
 
-bool arm(AnalogOut vel) {
+bool arm_raw(AnalogOut vel) {
 	int16_t pos  = analog_adc_get(ANA_POT_ARM);
 	bool    up   = vel > 0 && pos < ANA_POT_ARM_HIGH;
 	bool    down = vel < 0 && pos > ANA_POT_ARM_LOW;
@@ -34,7 +34,7 @@ bool arm(AnalogOut vel) {
 	return !move;
 }
 
-bool ramp(AnalogOut vel) {
+bool ramp_raw(AnalogOut vel) {
 	bool up   = vel > 0 && digital_get(DIG_LIM_LIFT_H);
 	bool down = vel < 0 && digital_get(DIG_LIM_LIFT_L);
 	bool move = up || down;
@@ -49,15 +49,9 @@ int32_t drive_straight(AnalogOut forward) {
 	int32_t diff  = ABS(left - right);
 	int32_t error = PROP(forward, DRIVE_STRAIGHT_ERRMAX, diff);
 
-	/* Veering to the right; turn counter-clockwise (left) to compensate. */
-	if (left > right) {
-		drive(kMotorMax, 0, error);
-	}
-	/* Veering to the left; turn clockwise (right) to compensate. */
-	else {
-		drive(kMotorMax, 0, -error);
-	}
-	return MIN(left, -right);
+	drive_raw(kMotorMax, 0, SIGN(left - right) * error);
+
+	return (left - right) / 2;
 }
 
 void arm_set(uint16_t tar) {
@@ -65,9 +59,5 @@ void arm_set(uint16_t tar) {
 	uint16_t diff = ABS((int16_t)tar - (int16_t)cur);
 	int8_t   out  = PROP(kMotorMax, ARM_SET_ERRMAX, diff);
 
-	if (cur < tar) {
-		arm(out);
-	} else {
-		arm(-out);
-	}
+	arm_raw(SIGN(tar - cur) * out);
 }
--- nitish/robot.h
+++ nitish/robot.h
@@ -11,19 +11,19 @@
  * strafe, and rotation. Positive values indicate forward movement, right
  * strafing and counter-clockwise rotation.
  */
-void drive(AnalogOut, AnalogOut, AnalogOut);
+void drive_raw(AnalogOut, AnalogOut, AnalogOut);
 
 /* Control the speed of the arm motors to raise or lower the arm. Positive
  * values indicate upward movement. Both extremes are checked in software using
  * a potentiometer.
  */
-bool arm(AnalogOut);
+bool arm_raw(AnalogOut);
 
 /* Control the speed of the ramp motor(s) to raise or lower the ramp. Positive
  * values indicate upward movement. Both extremes are checked in software using
  * limit switches.
  */
-bool ramp(AnalogOut);
+bool ramp_raw(AnalogOut);
 
 /* Drive forward or reverse in a straight line, using encoders to correct for
  * small errors.
