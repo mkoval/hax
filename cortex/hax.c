76bbf2df6e2f95a37f05794f9fc4b6a6e1a46930
Michael Koval <koval.michael@gmail.com>
2010-03-17 18:32:22 -0400

    Added empty initialization functions that are unused with the EasyC library. Next up: implementation of HAX for the Cortex.

--- cortex/hax.c
+++ cortex/hax.c
@@ -1,7 +1,31 @@
 #include "../hax.h"
 
 void IO_Initialization(void) {
-	SetCompetitionMode ( -1, -1 ) ;
-	DefineControllerIO ( 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 ) ;
+    /* Enable VexNET control in the EasyC library. */
+	SetCompetitionMode(-1, -1);
+
+    /* ??? */
+	DefineControllerIO(1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0);
 }
+
+void setup_1(void) {
+    /* Handled by the EasyC library. */
+}
+
+void setup_2(void) {
+    /* Handled by the EasyC library. */
+}
+
+void spin(void) {
+}
+
+void loop_1(void) {
+    /* Handled by the EasyC library. */
+}
+
+void loop_2(void) {
+    /* Handled by the EasyC library. */
+}
+
+
 
