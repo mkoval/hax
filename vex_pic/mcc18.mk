1173d55fa054cea6a068b9d7a5b9bfd73ec487c5
Michael Koval <koval.michael@gmail.com>
2010-03-17 20:31:03 -0400

    Updated the vex_pic architecture to match the new Makefile scheme.

--- vex_pic/mcc18.mk
+++ vex_pic/mcc18.mk
@@ -1,20 +1,39 @@
+MCCPATH      = /opt/mcc18
+WINPATH      = $(srcdir)/vex_pic/winpath.sh
+CC           = '$(MCCPATH)/bin/mcc18'
+LD           = '$(MCCPATH)/bin/mplink'
+AS           = '$(MCCPATH)/mpasm/mpasm'
+IPATH        = '$(MCCPATH)/h'
+WIPATH      := '$(shell $(WINPATH) $(IPATH))'
+ARCH_CFLAGS  = -I=$(WIPATH) -p=18F8520
+ARCH_AFLAGS  = /p18f8520
+LIBPATH      = '$(MCCPATH)/lib'
+WLIBPATH    := '$(shell $(WINPATH) $(LIBPATH))'
+ARCH_LDFLAGS = $(ARCH)/18f8520user.lkr /l $(WLIBPATH) /a INHX32 
 
-MCCPATH   = /opt/mcc18
-WINPATH   = $(srcdir)/vex_pic/winpath.sh
-CC        = '$(MCCPATH)/bin/mcc18'
-LD        = '$(MCCPATH)/bin/mplink'
-AS        = '$(MCCPATH)/mpasm/mpasm'
-IPATH     = '$(MCCPATH)/h'
-WIPATH   := '$(shell $(WINPATH) $(IPATH))'
-ARCH_CFLAGS = -I=$(WIPATH) -p=18F8520
-ARCH_AFLAGS = /p18f8520
-LIBPATH   = '$(MCCPATH)/lib'
-WLIBPATH := '$(shell $(WINPATH) $(LIBPATH))'
-#VEX_LIB   = $(ARCH)/Vex_alltimers.lib
-LD_SCRIPT = $(ARCH)/18f8520user.lkr
-ARCH_LDFLAGS = $(VEX_LIB) /l $(WLIBPATH) /a INHX32 
-LDOUT_OPT = /o
-CCOUT_OPT = -fo=
+OBJECTS = $(CSOURCE:=.o) $(ASOURCE:=.o)
+TRASH  += $(TARGET:.hex=.cod) $(TARGET:.hex=.lst) $(OBJECTS:.o=.err)
 
-TRASH     = $(TARGET:.hex=.cod) $(TARGET:.hex=.lst) $(OBJECTS:.o=.err)
+.SECONDARY : 
 
+all : $(TARGET)
+
+rebuild : clean all
+
+clean :
+	@echo "CLEAN"
+	@$(RM) $(OBJECTS) $(TARGET) $(TRASH)
+
+$(TARGET) : $(OBJECTS)
+	@echo "LD $(@F)"
+	@$(LD) $(ALL_LDFLAGS) $^ /o$@
+
+%.c.o : %.c $(HEADERS) 
+	@echo "CC $(@F)"
+	@$(CC) $(ALL_CFLAGS) $< -fo=$@
+
+%.asm.o : %.asm $(HEADERS)
+	@echo "AS $(@F)"
+	@$(AS) /q $(ALL_AFLAGS) $< /o$@
+
+.PHONY : all clean install rebuild
