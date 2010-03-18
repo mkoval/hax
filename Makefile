a33fbd4bff6f7610521551398bdab52888dd9cf0
Michael Koval <koval.michael@gmail.com>
2010-03-17 21:17:09 -0400

    Finished the cortex-specific Makefiles.

--- Makefile
+++ Makefile
@@ -4,15 +4,15 @@ srcdir = .
 VPATH  = $(srcdir)
 
 PROG = skel
-ARCH = vex_pic
+ARCH = cortex
 
 CSOURCE = hax_main.c hax_serial.c
 HEADERS = stdint.h hax.h
 
-include $(PROG)/Makefile
-include $(ARCH)/Makefile
-
 ALL_CFLAGS  = $(ARCH_CFLAGS) $(CFLAGS)
 ALL_LDFLAGS = $(ARCH_LDFLAGS) $(LDFLAGS)
 ALL_AFLAGS  = $(ARCH_AFLAGS) $(AFLAGS)
+
+include $(PROG)/Makefile
+include $(ARCH)/Makefile
 
--- cortex/gcc.mk
+++ cortex/gcc.mk
@@ -1,14 +1,43 @@
-CC           = 'arm-none-eabi-gcc'
-LD           = 'arm-none-eabi-gcc'
-AS           = 'arm-none-eabi-as'
-CCOUT_OPT    = -o 
-LDOUT_OPT    = -o 
+CC      = 'arm-none-eabi-gcc'
+LD      = 'arm-none-eabi-gcc'
+AS      = 'arm-none-eabi-as'
+OBJCOPY = 'arm-none-eabi-objcopy'
+
 ARCH_CFLAGS  = -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_ -mthumb -mcpu=cortex-m3  \
                -fsigned-char -ffunction-sections -Wall -mlittle-endian         \
                -Wno-main -c
-ARCH_AFLAGS  =
 ARCH_LDFLAGS = -Wl,-T -Xlinker 'cortex/vexlib/Scripts/easyCRuntimeInit.elf.ld' \
                -L'cortex/vexlib/Startup' -L'cortex/vexlib/easyCRuntime'        \
                -L'cortex/vexlib/Link' -mpu=cortex-m3 -mthumb -u _start         \
                -Wl,-static -Wl,--gc-sections -nostartfiles
-TRASH        = $(CSOURCE:.c=.c.d)
+
+OBJECTS = $(CSOURCE:.c=.c.o)
+TRASH   = $(OBJECTS)
+
+.SECONDARY : 
+
+all : $(TARGET)
+
+rebuild : clean all
+
+clean :
+	@echo "CLEAN"
+	@$(RM) $(OBJECTS) $(TARGET) $(TARGET_ELF) $(TRASH)
+
+$(TARGET_ELF) : $(OBJECTS)
+	@echo "LD $(@F)"
+	@$(LD) $(ALL_LDFLAGS) $^ -o $@
+
+%.hex : %.elf
+	@echo "HEX $(@F)"
+	@$(OBJCOPY) $< --target=ihex $@
+
+%.c.o : %.c $(HEADERS) 
+	@echo "CC $(@F)"
+	@$(CC) $(ALL_CFLAGS) $< -o $@
+
+%.asm.o : %.asm $(HEADERS)
+	@echo "AS $(@F)"
+	@$(AS) /q $(ALL_AFLAGS) $< -o $@
+
+.PHONY : all clean install rebuild
