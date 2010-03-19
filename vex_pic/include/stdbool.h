f215af2d14ead5e920425079da86ad0ea4ca71c2
John Mesmon <jmesmon@gmail.com>
2010-03-19 02:16:41 -0400

    clean up vex_pic's std{int,bool}

--- vex_pic/include/stdbool.h
+++ vex_pic/include/stdbool.h
@@ -1,9 +1,16 @@
 #ifndef BOOL_H_
 #define BOOL_H_
 
+#include "compilers.h"
+
+#if defined(MCC18_24)
 typedef unsigned char bool;
 #define true 1
 #define false 0
+
+#else
+#error "MCC18 STDBOOL failed"
+#endif
 
 
 #endif
--- vex_pic/include/stdint.h
+++ vex_pic/include/stdint.h
@@ -1,20 +1,25 @@
 #ifndef STDINT_H_
 #define STDINT_H_
 
+#include "compilers.h"
+
 /* As defined in Section 2.1, Page 11 of the MCC18 C18 User Guide */
-#if defined(__18CXX)
+#if defined(MCC18_24)
 typedef signed   char int8_t;
 typedef unsigned char uint8_t;
 
 typedef signed   int int16_t;
 typedef unsigned int uint16_t;
 
+typedef signed   short long int24_t;
+typedef unsigned short long uint24_t;
+
 typedef signed   long int32_t;
 typedef unsigned long uint32_t;
 
-#elif defined(__ARM_EABI__) && defined(__GNUC__)
+#else
 
-#include <stdint.h>
+#error "MCC18 STDINT failed"
 
 #endif
 
