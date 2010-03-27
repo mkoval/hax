b18fbdb5bd358f6008f48f1b4909f3aec77ec395
Michael Koval <koval.michael@gmail.com>
2010-03-27 03:50:52 -0400

    Added auto-dependancy generation to the MCC18 Makefile. Currently functional, but imperfect.

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ ARCH        = vex_pic
 ALL_CFLAGS  = $(ARCH_CFLAGS) $(CFLAGS)
 ALL_LDFLAGS = $(ARCH_LDFLAGS) $(LDFLAGS)
 ALL_ASFLAGS = $(ARCH_ASFLAGS) $(ASFLAGS)
+ALL_MDFLAGS = $(ARCH_MDFLAGS) $(MDFLAGS)
 
 SOURCE      = hax_main.c hax_serial.c
 HEADERS     = hax.h
--- vex_pic/mcc18.mk
+++ vex_pic/mcc18.mk
@@ -1,6 +1,7 @@
 CC           = '$(MCCPATH)/bin/mcc18'
 LD           = '$(MCCPATH)/bin/mplink'
 AS           = '$(MCCPATH)/mpasm/mpasm'
+MD           = gcc
 
 MCCPATH      = /opt/mcc18
 WINPATH      = $(srcdir)/vex_pic/winpath.sh
@@ -14,9 +15,14 @@ WLIBPATH    := '$(shell $(WINPATH) $(LIBPATH))'
 ARCH_CFLAGS  = -I=$(WICPATH) -I=$(WIPATH) -I=$(srcdir) -p=18F8520
 ARCH_ASFLAGS = /p18f8520
 ARCH_LDFLAGS = $(ARCH)/18f8520user.lkr /l $(WLIBPATH) /a INHX32
+ARCH_MDFLAGS = -MM -DMCC18_24 -D__18CXX -DUNCHANGEABLE_DEFINITION_AREA \
+               -L$(srcdir)
 
 OBJECTS     += $(SOURCE:=.o)
-TRASH       += $(TARGET:.hex=.cod) $(TARGET:.hex=.lst) $(OBJECTS:.o=.err)
+TRASH       += $(TARGET:.hex=.cod) \
+               $(TARGET:.hex=.lst) \
+               $(OBJECTS:.o=.err)  \
+               $(OBJECTS:.o=.d)
 
 .SUFFIXES:
 .SECONDARY:
@@ -33,11 +39,19 @@ $(TARGET) : $(OBJECTS)
 	@echo "LD $(@F)"
 	@$(LD) $(ALL_LDFLAGS) $^ /o$@
 
-%.c.o : %.c $(HEADERS) 
+-include $(OBJECTS:.o=.d)
+
+%.c.o : %.c
 	@echo "CC $(@F)"
 	@$(CC) $(ALL_CFLAGS) $< -fo=$@
 
-%.asm.o : %.asm $(HEADERS)
+    # Generate auto-dependancy files using GCC.
+	@$(MD) $(ALL_MDFLAGS) $< -o $<.d
+	@mv -f $<.d $<.tmp
+	@sed -e 's|.*:|$<.o:|' < $<.tmp > $<.d
+	@$(RM) $<.tmp
+
+%.asm.o : %.asm
 	@echo "AS $(@F)"
 	@$(AS) /q $(ALL_ASFLAGS) $< /o$@
 
