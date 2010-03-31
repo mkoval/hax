64105d1bf416a67db681886137401f681817a5ba
Michael Koval <koval.michael@gmail.com>
2010-03-31 03:25:24 -0400

    Implemented a skeleton wrapper for the vex_cortex architecture. Builds successfully, but is extremely messy.

--- vex_cortex/hax.c
+++ vex_cortex/hax.c
@@ -1,48 +1,224 @@
 #include <hax.h>
 #include <stdbool.h>
+#include <string.h>
 
-static Slave_Spi_Record  *slave;
-static Master_Spi_Record *master;
+#include "fwlib/inc/stm32f10x_spi.h"
+#include "fwlib/inc/stm32f10x_type.h"
+#include "fwlib/inc/stm32f10x_gpio.h"
+#include "vexlib/platform_config.h"
 
-void setup_1(void) {
-	RCC_Configuration();
-	NVIC_Configuration();
-	GPIO_Configuration();
-	ADC_Config();
-	USART_Configuration();
-	SysTick_Configuration();
-	Setup_Capture_Mode();
-	Setup_timer1();
-	Setup_timer4();
+/*
+ * VEXLIB IMPLEMENTATION
+ */
 
-	WaitTillMasterIsReadyForSpiData();
+/* Temporary constants. */
+#define MAX_MOTORS 8
+#define BufferSize 32
+
+Slave_Spi_Record  *slavePtr;
+Master_Spi_Record *masterPtr;
+
+static uint8_t crystalActive = 0;
+
+uint16_t SPI1_Buffer_Rx[BufferSize];
+uint16_t SPI1_Buffer_Tx[BufferSize];
+
+uint16_t rx1[BufferSize];
+uint16_t rx2[BufferSize];
+
+uint8_t dataFromPC = 0;
+uint8_t sendit = 0;
+uint8_t rx1Timeout = 0;
+uint8_t rx2Timeout = 0;
+uint8_t systemStateFlags = 0;
+
+int analogMask; /* TODO Use a explicitly sized data type. */
+
+volatile uint16_t ADCConvertedValue[10];
+
+/* Definitions that should be in the Vex headers. */
+extern u16 pwm1[];
+extern u16 pwm2[];
+extern uint8_t pwmStatusFlags;
+extern uint8_t SendDataToUart;
+extern uint8_t updateMaster;
+
+void Debug_Send(void);
+
+void Handle_Data_From_Crystal(u8 receiverPort) {
+	u8 i, start;
+	u16 *pwmPtr;
+	u16 *rxPtr;
+	float fPWM;
+
+	//crystalActive |= receiverPort;
+	if (receiverPort == 1)	//RX1?
+	{
+		pwmPtr = &pwm1[0];
+		rxPtr = &rx1[0];
+		pwmStatusFlags &= ~RX1_INTRDY;
+		pwmStatusFlags |= RX1_DTARDY;
+		rx1Timeout = 5;	//(5 * 18ms)
+	} else			//must be RX2
+	{
+		pwmPtr = &pwm2[0];
+		rxPtr = &rx2[0];
+		pwmStatusFlags &= ~RX2_INTRDY;
+		pwmStatusFlags |= RX2_DTARDY;
+		rx2Timeout = 5;	//(5 * 18ms)
+	}
+	start = 1;
+	if ((systemStateFlags & 8) == 8)
+		start = 0;
+	for (i = start; i < 7; i++) {
+		rxPtr[i] = pwmPtr[i];
+		fPWM = (float) pwmPtr[i] / 14.0;
+		rxPtr[i] = (u16) fPWM;
+		if (rxPtr[i] > 255)
+			rxPtr[i] = 255;
+		if ((rxPtr[i] >= 123) && (rxPtr[i] <= 131))
+			rxPtr[i] = 127;
+	}
 }
 
-void setup_2(void) {
+void Invoke_SPI_Transfer(void) {
+	static u8 packetNum = 0;
+	int i, j, gap;
+
+	GPIO_SetBits(GPIOA, GPIO_Pin_11);	//RTS high Used to ensure 1st 4 bytes
+	gap = 0;
+	for (j = 0; j < SPI_PACKET_SIZE; j++) {
+		GPIO_ResetBits(GPIOE, GPIO_Pin_0);	//Use as SSL0
+		while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) ==
+		       RESET);
+		SPI_I2S_SendData(SPI1, SPI1_Buffer_Tx[j]);
+
+		while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) ==
+		       RESET);
+		SPI1_Buffer_Rx[j] = SPI_I2S_ReceiveData(SPI1);
+
+		while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) ==
+		       RESET);
+		GPIO_SetBits(GPIOE, GPIO_Pin_0);	//Use as SSL0
+		for (i = 0; i < 150; i++);	//15us
+		gap++;
+		if (gap == 4)	//put a gap after 4 bytes xfered  
+		{
+			for (i = 0; i < 1000; i++);	//210us
+			GPIO_ResetBits(GPIOA, GPIO_Pin_11);	//RTS low
+			gap = 0;
+		}
+	}
+	slavePtr->packetNum = packetNum++;
 }
 
-void loop_1(void) {
+void WaitTillMasterIsReadyForSpiData(void) {
+	int temp = temp;
+	int j;
+	int masterPtrNotReady = true;
+	int delay = 0;
+
+	GPIO_SetBits(GPIOE, GPIO_Pin_0);	//SPI1_INT
+	for (j = 0; j < 8; j++)
+		temp = SPI_I2S_ReceiveData(SPI1);
+	for (j = 0; j < SPI_PACKET_SIZE; j++)
+		SPI1_Buffer_Tx[j] = 127;
+
+	while (masterPtrNotReady)	//Loop Here until Master is ready to handle SPI data
+	{
+		Delay(200);
+		if (!GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_3))	//PROG_SEL1
+			if (!GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_4))	//PROG_SEL2
+				masterPtrNotReady = false;
+		delay++;
+		if (delay > 50) {
+			delay = 0;
+		}
+	}
 }
 
-void loop_2(void) {
+void Set_MotorControl_Sw1(uint8_t data) {
+	uint8_t newValue;
+
+	if (data > 129) {
+		TIM4->CCR2 = 0;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_3);	//AH1 
+		newValue = data - 128;
+		GPIO_SetBits(GPIOD, GPIO_Pin_4);	//BH1 
+		TIM4->CCR1 = newValue;
+	} else if (data < 126) {
+		TIM4->CCR1 = 0;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_4);	//BH1 
+		newValue = 128 - data;
+		GPIO_SetBits(GPIOD, GPIO_Pin_3);	//AH1 
+		TIM4->CCR2 = newValue;
+	} else {
+		TIM4->CCR1 = 0;
+		TIM4->CCR2 = 0;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_3);	//AH1
+		GPIO_ResetBits(GPIOD, GPIO_Pin_4);	//BH1 
+	}
 }
 
-CtrlMode mode_get(void) {
-	return 0;
+void Set_MotorControl_Sw2(u8 data) {
+	uint8_t newValue;
+
+	if (data > 129) {
+		TIM4->CCR4 = 0;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_7);	//AH2 
+		newValue = data - 128;
+		GPIO_SetBits(GPIOD, GPIO_Pin_8);	//BH2 
+		TIM4->CCR3 = newValue;
+	} else if (data < 126) {
+		TIM4->CCR3 = 0;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_8);	//BH2 
+		newValue = 128 - data;
+		GPIO_SetBits(GPIOD, GPIO_Pin_7);	//AH2 
+		TIM4->CCR4 = newValue;
+	} else {
+		TIM4->CCR3 = 0;
+		TIM4->CCR4 = 0;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_7);	//AH2 
+		GPIO_ResetBits(GPIOD, GPIO_Pin_8);	//BH2 
+	}
 }
 
-bool new_data_received(void) {
-	return false;
+void Handle_Autonomous(void) {
+	int i;
+
+	for (i = 0; i < MAX_MOTORS; i++)	//Update Motors
+	{
+		if (dataFromPC == 0)	//not using DDT?
+			slavePtr->Motor[i] = ADCConvertedValue[i] >> 2;
+		slavePtr->MotorStatus[i] = 0;
+		slavePtr->Analog[i] = 0;
+	}
+	Set_MotorControl_Sw1(slavePtr->Motor[0]);
+	Set_MotorControl_Sw2(slavePtr->Motor[1]);
+}
+
+void Set_MotorControl_To_Neutral(void) {
+	GPIO_ResetBits(GPIOD, GPIO_Pin_3);	//AH1
+	GPIO_ResetBits(GPIOD, GPIO_Pin_4);	//BH1 
+	GPIO_ResetBits(GPIOD, GPIO_Pin_7);	//AH2 
+	GPIO_ResetBits(GPIOD, GPIO_Pin_8);	//BH2 
+
+	GPIO_SetBits(GPIOD, GPIO_Pin_12);	//~AL1 
+	GPIO_SetBits(GPIOD, GPIO_Pin_13);	//~BL1
+	GPIO_SetBits(GPIOD, GPIO_Pin_14);	//~AL2 
+	GPIO_SetBits(GPIOD, GPIO_Pin_15);	//~BL2 
+}
+
+void Handle_Operator_Control() {
+	/* TODO Implement this. */
 }
 
 /*
  * INITIALIZATION AND MISC
  */
 void setup_1(void) {
-	AnalogOutIx i, j;
-	uint8_t delay, sendit;
-	int uart1data = uart1data;  //to ignore compile warning
-	float b1, m2;
+	AnalogOutIx i;
+	int uart1data = uart1data; /* Hack to get around a GCC error. */
 
 	RCC_Configuration();
 	NVIC_Configuration();
@@ -56,18 +232,18 @@ void setup_1(void) {
 
 	WaitTillMasterIsReadyForSpiData(); 
 
-	/* Initialize communication with the master processor. */
-	slave  = (Slave_Spi_Record *)  &SPI1_Buffer_Tx[0];
-	master = (Master_Spi_Record *) &SPI1_Buffer_Rx[0];
-	memset(slave, 0, sizeof(Slave_Spi_Record));
+	/* Initialize communication with the masterPtr processor. */
+	slavePtr  = (Slave_Spi_Record *)  &SPI1_Buffer_Tx[0];
+	masterPtr = (Master_Spi_Record *) &SPI1_Buffer_Rx[0];
+	memset(slavePtr, 0, sizeof(Slave_Spi_Record));
 
-	slave->Sync         = 0xC917;
-	slave->State        = 2; /* Bitmask; config flag. */
-	slave->SystemFlags  = 0; /* Let the master processor control modes. */
-	slave->DigitalByte1 = 0;
-	slave->DigitalByte2 = 0;
-	slave->version      = 1; /* Slave version number. */
-	slave->packetNum    = 0;
+	slavePtr->Sync         = 0xC917;
+	slavePtr->State        = 2; /* Bitmask; config flag. */
+	slavePtr->SystemFlags  = 0; /* Let the masterPtr processor control modes. */
+	slavePtr->DigitalByte1 = 0;
+	slavePtr->DigitalByte2 = 0;
+	slavePtr->version      = 1; /* Slave version number. */
+	slavePtr->packetNum    = 0;
 
 	/* Set motor control to neutral. */
 	GPIO_ResetBits(GPIOD, GPIO_Pin_3);
@@ -82,7 +258,7 @@ void setup_1(void) {
 
 	/* Initialize the motors to neutral. */
 	for (i = 0; i < MAX_MOTORS; ++i) {
-		slave->Motor[i]      = 127;
+		slavePtr->Motor[i]      = 127;
 		ADCConvertedValue[i] = 100; /* ? */
 	}
 
@@ -95,9 +271,19 @@ void setup_1(void) {
 	analogMask = 0; /* ? */
 }
 
-void setup_2(void);
+void setup_2(void) {
+}
+
+void loop_1(void) {
+}
+
+void loop_2(void) {
+}
 
 void spin(void) {
+	static uint8_t one_shot = 1;
+	int uart1data = uart1data; /* Hack to get around a GCC error. */
+
 #ifdef USE_USART2
 	/* Check for data transmitted over wifi. */
 	uart1data = GetKey();
@@ -118,12 +304,8 @@ void spin(void) {
 	if (pwmStatusFlags & RX2_INTRDY) {
 		Handle_Data_From_Crystal(2);
 	}
-}
 
-void spin(void) {
-	static uint8_t one_shot = 1;
-
-	if (!sendit && SendDataToUart()) {
+	if (!sendit && SendDataToUart) {
 		Debug_Send(); /* ? */
 	}
 
@@ -132,7 +314,7 @@ void spin(void) {
 	}
 	/* Timeout waiting for data from RX1. */
 	else {
-		pwmStatusFlags &= ~RX_DTARDY;
+		pwmStatusFlags &= ~RX1_DTARDY;
 		crystalActive  &= ~1;
 	}
 
@@ -145,63 +327,40 @@ void spin(void) {
 		crystalActive  &= ~2;
 	}
 
-	/* Invoke the SPI transfer between the master and slave. */
-	GPIO_SetBits(GPIOA, GPIO_Pin_11); /* ? */
-
-	for (i = 0; i < SPI_PACKET_SIZE; ++i) {
-		GPIO_ResetBits(GPIOE, GPIO_Pin_0);
-
-		while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-		SPI_I2S_SendData(SPI1, SPI1_Buffer_Tx[j]);
-
-		while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
-		SPI1_Buffer_Rx[j] = SPI_I2S_ReceiveData(SPI1);
-
-		while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-		GPIO_SetBits(GPIOE, GPIO_Pin_0);
-
-		/* Wait for 15 microseconds. */
-		/* TODO: Replace this with something less hacky; maybe noops? */
-		for (i = 0; i < 150; ++i);
-
-		
-
+	/* Invoke the SPI transfer between the masterPtr and slavePtr. */
 	Invoke_SPI_Transfer();
 
-	/* Valid sync with the master processor. */
-	if (master->Sync == 0xC917) {
-		/* Handle master-slave configuration. */
-		if (master->State & 2) {
+	/* Valid sync with the masterPtr processor. */
+	if (masterPtr->Sync == 0xC917) {
+		/* Handle masterPtr-slavePtr configuration. */
+		if (masterPtr->State & 2) {
 			/* TODO Replace this state machine with something in init. */
 
 			/* Send the configuration IACK once. */
 			if (one_shot) {
-				slave->State = 3;
+				slavePtr->State = 3;
 			}
-			/* If the master IACK's, the slave is ready for data. */
-			else if (master->State & 1) {
-				slave->State = 8;
+			/* If the masterPtr IACK's, the slavePtr is ready for data. */
+			else if (masterPtr->State & 1) {
+				slavePtr->State = 8;
 			}
 			one_shot = 0;
 		}
 		/* Wait for an instruction to complete. */
-		else if (master->State & 4) {
-			slave->State = 8;      /* Slave has data ready. */
-			master->packetNum = 1; /* ? */
+		else if (masterPtr->State & 4) {
+			slavePtr->State = 8;      /* Slave has data ready. */
+			masterPtr->packetNum = 1; /* ? */
 		}
 		/* Receied valid data; update autonomous mode. */
-		else if (master->State == 8) {
-			/* Autonomous is either forced by master or was set by slave. */
-			if (master->SystemFlags & 0x40 || slave->SystemFlags == 1) {
+		else if (masterPtr->State == 8) {
+			/* Autonomous is either forced by masterPtr or was set by slavePtr. */
+			if (masterPtr->SystemFlags & 0x40 || slavePtr->SystemFlags == 1) {
 				Handle_Autonomous();
 			} else {
 				Handle_Operator_Control();
 			}
 		}
 	}
-}
-
-void loop_2(void) {
 }
 
 bool new_data_received(void) {
@@ -214,8 +373,9 @@ bool new_data_received(void) {
 }
 
 CtrlMode mode_get(void) {
+	return 0;
 }
 
-void mode_set(CtrlMode) {
+void mode_set(CtrlMode mode) {
 }
 
