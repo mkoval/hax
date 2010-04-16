1df9e9d9756520ca4eaffed6d87f87f7dcbd7c6e
Michael Koval <koval.michael@gmail.com>
2010-04-16 18:22:52 -0400

    Implemented a simple autonomous mode for the Nitish.

--- kevin/state.h
+++ kevin/state.h
@@ -7,6 +7,8 @@
 
 static bool kFalse = 0;
 
+#if defined(ROBOT_KEVIN)
+
 STATE_START()
 STATE(deploy,      AUTO_ARM(250, 100, 127),       auto_arm_init,      auto_arm_loop,      init_ram,    init_ram, kFalse)
 STATE(init_ram,    AUTO_RAM(2000, 127),           auto_straight_init, auto_straight_loop, init_arm,    done,     auto_ram_isdone(state->data))
@@ -40,5 +42,27 @@ transition_t auto_cbs[_st_end - _st_start - 1] = {
 	STATE_LOOKUP(coll_dump3),
 	STATE_LOOKUP(done)
 };
+
+#elif defined(ROBOT_NITISH)
+
+STATE_START()
+STATE(init_ram,    AUTO_RAM(2000, 127),           auto_straight_init, auto_straight_loop, init_arm,    done, auto_ram_isdone(state->data))
+STATE(init_arm,    AUTO_ARM(500, 0,   127),       auto_arm_init,      auto_arm_loop,      init_dump1,  done, auto_arm_isdone(state->data))
+STATE(init_dump1,  AUTO_RAMP(500, 100, 127),      auto_ramp_init,     auto_ramp_loop,     init_dump2,  done, auto_ramp_isdone(state->data)) 
+STATE(init_dump2,  AUTO_WAIT(1000),               auto_none_init,     auto_none_loop,     init_dump3,  done, auto_none_isdone(state->data))
+STATE(init_dump3,  AUTO_RAMP(500, 0, 127),        auto_ramp_init,     auto_ramp_loop,     done,        done, auto_ramp_isdone(state->data))
+STATE(done,        AUTO_WAIT(1),                  auto_none_init,     auto_none_loop,     done,        done, kFalse)
+STATE_DONE()
+
+transition_t auto_cbs[_st_end - _st_start - 1] = {
+	STATE_LOOKUP(init_ram),
+	STATE_LOOKUP(init_arm),
+	STATE_LOOKUP(init_dump1),
+	STATE_LOOKUP(init_dump2),
+	STATE_LOOKUP(init_dump3),
+	STATE_LOOKUP(done)
+};
+
+#endif
 
 #endif
