35ce36541a48432a55993f0d9e13058156b511d5
Michael Koval <koval.michael@gmail.com>
2010-04-18 03:38:20 -0400

    Modified the autonomous framework to move timeout from data_t to state_t and use one-line state definitions.

--- comp2010/auton.c
+++ comp2010/auton.c
@@ -8,69 +8,88 @@
 
 #include "auton.h"
 
+void auto_deploy_init(state_t const __rom *state) {
+}
+
+void auto_deploy_loop(state_t const __rom *state) {
+	arm_raw(state->data->pose.vel);
+}
+
 /* Drive straight using encoders for velocity control. */
-void auto_straight_init(data_t *data) {
-	data->move.enc_left  = encoder_get(ENC_L);
-	data->move.enc_right = encoder_get(ENC_R);
+void auto_straight_init(state_t const __rom *state) {
+	state->data->move.enc_left  = encoder_get(ENC_L);
+	state->data->move.enc_right = encoder_get(ENC_R);
+
+	printf((char *)"target = %5d\n\r", (int)(state->data->move.ticks));
 }
 
-void auto_straight_loop(data_t *data) {
-	int32_t left  = encoder_get(ENC_L) - data->move.enc_left;
-	int32_t right = encoder_get(ENC_R) - data->move.enc_right;
+void auto_straight_loop(state_t const __rom *state) {
+	int32_t left  = encoder_get(ENC_L) - state->data->move.enc_left;
+	int32_t right = encoder_get(ENC_R) - state->data->move.enc_right;
 	int32_t diff  = left - right;
-	int8_t  turn  = PROP(ABS(data->move.vel), STRAIGHT_ERROR, ABS(diff));
+	int8_t  turn  = PROP(ABS(state->data->move.vel), STRAIGHT_ERROR, ABS(diff));
 
-	drive_smart(data->move.vel, SIGN(diff) * turn);
+#if DEBUG
+	printf((char *)"delta = %5d, correct = %3d\n\r", (int)diff, (int)turn);
+#endif
+
+	drive_smart(state->data->move.vel, SIGN(diff) * turn);
 }
 
-bool auto_straight_isdone(data_t *data) {
-	int32_t left  = encoder_get(ENC_L) - data->move.enc_left;
-	int32_t right = encoder_get(ENC_R) - data->move.enc_right;
+bool auto_straight_isdone(state_t const __rom *state) {
+	int32_t left  = encoder_get(ENC_L) - state->data->move.enc_left;
+	int32_t right = encoder_get(ENC_R) - state->data->move.enc_right;
 
-	return ABS(left + right) >= 2 * data->move.ticks;
+#if DEBUG
+	printf((char *)"travelled = %5d, target = %5d\n\r",
+	       (int)(ABS(left + right) / 2), (int)(state->data->move.ticks));
+#endif
+
+	return ABS(left + right) >= 2 * state->data->move.ticks;
 }
 
 /* Turn through a specified angle using encoders as velocity control. */
-void auto_turn_init(data_t *data) {
+void auto_turn_init(state_t const __rom *state) {
 	/* Store the initial encoder counts for both wheels. */
-	data->move.enc_left  = encoder_get(ENC_L);
-	data->move.enc_right = encoder_get(ENC_R);
+	state->data->move.enc_left  = encoder_get(ENC_L);
+	state->data->move.enc_right = encoder_get(ENC_R);
 }
 
-void auto_turn_loop(data_t *data) {
-	int32_t left  = encoder_get(ENC_L) - data->move.enc_left;
-	int32_t right = encoder_get(ENC_R) - data->move.enc_right;
-	int32_t diff  = ABS(left - right) / 2 - data->move.ticks;
-	int8_t  turn  = PROP(ABS(data->move.vel), TURN_ERROR, ABS(diff));
+void auto_turn_loop(state_t const __rom *state) {
+	int32_t left  = encoder_get(ENC_L) - state->data->move.enc_left;
+	int32_t right = encoder_get(ENC_R) - state->data->move.enc_right;
+	int32_t diff  = ABS(left - right) / 2 - state->data->move.ticks;
+	int8_t  turn  = 50 + PROP(MAX(ABS(state->data->move.vel), 50) - 50,
+	                          TURN_ERROR, ABS(diff));
 
-	drive_smart(0, SIGN(data->move.vel) * turn);
+	drive_smart(0, SIGN(state->data->move.vel) * turn);
 }
 
-bool auto_turn_isdone(data_t *data) {
-	int32_t left  = encoder_get(ENC_L) - data->move.enc_left;
-	int32_t right = encoder_get(ENC_R) - data->move.enc_right;
-	int32_t diff  = ABS(left - right) / 2 - data->move.ticks;
+bool auto_turn_isdone(state_t const __rom *state) {
+	int32_t left  = encoder_get(ENC_L) - state->data->move.enc_left;
+	int32_t right = encoder_get(ENC_R) - state->data->move.enc_right;
+	int32_t diff  = ABS(left - right) / 2 - state->data->move.ticks;
 
 	return ABS(diff) < TURN_THRESHOLD;
 }
 
 /* Rotate the arm to be in the specified position. */
-void auto_arm_init(data_t *data) {
+void auto_arm_init(state_t const __rom *state) {
 }
 
-void auto_arm_loop(data_t *data) {
+void auto_arm_loop(state_t const __rom *state) {
 	int16_t pos  = analog_adc_get(POT_ARM);
-	bool    up   = data->pose.vel > 0 && ARM_LT(pos, POT_ARM_HIGH);
-	bool    down = data->pose.vel < 0 && ARM_GT(pos, POT_ARM_LOW);
+	bool    up   = state->data->pose.vel > 0 && ARM_LT(pos, POT_ARM_HIGH);
+	bool    down = state->data->pose.vel < 0 && ARM_GT(pos, POT_ARM_LOW);
 	bool    move = up || down;
 
-	arm_raw(move * data->pose.vel);
+	arm_raw(move * state->data->pose.vel);
 }
 
-bool auto_arm_isdone(data_t *data) {
+bool auto_arm_isdone(state_t const __rom *state) {
 	int16_t pos  = analog_adc_get(POT_ARM);
-	bool    up   = data->pose.vel > 0 && ARM_LT(pos, POT_ARM_HIGH);
-	bool    down = data->pose.vel < 0 && ARM_GT(pos, POT_ARM_LOW);
+	bool    up   = state->data->pose.vel > 0 && ARM_LT(pos, POT_ARM_HIGH);
+	bool    down = state->data->pose.vel < 0 && ARM_GT(pos, POT_ARM_LOW);
 
 	return !(up || down);
 }
@@ -78,33 +97,33 @@ bool auto_arm_isdone(data_t *data) {
 #if defined(ROBOT_KEVIN)
 
 /* Move the ramp into the specific position.  */
-void auto_ramp_init(data_t *data) {
+void auto_ramp_init(state_t const __rom *state) {
 }
 
-void auto_ramp_loop(data_t *data) {
+void auto_ramp_loop(state_t const __rom *state) {
 	int16_t pos  = analog_adc_get(POT_LIFT);
-	bool    up   = data->pose.vel > 0 && LIFT_LT(pos, POT_LIFT_HIGH);
-	bool    down = data->pose.vel < 0 && LIFT_GT(pos, POT_LIFT_LOW);
+	bool    up   = state->data->pose.vel > 0 && LIFT_LT(pos, POT_LIFT_HIGH);
+	bool    down = state->data->pose.vel < 0 && LIFT_GT(pos, POT_LIFT_LOW);
 	bool    move = up || down;
 
-	ramp_raw(move * data->pose.vel, move * data->pose.vel);
+	ramp_raw(move * state->data->pose.vel, move * state->data->pose.vel);
 }
 
-bool auto_ramp_isdone(data_t *data) {
+bool auto_ramp_isdone(state_t const __rom *state) {
 	int16_t pos  = analog_adc_get(POT_LIFT);
-	bool    up   = data->pose.vel > 0 && LIFT_LT(pos, POT_LIFT_HIGH);
-	bool    down = data->pose.vel < 0 && LIFT_GT(pos, POT_LIFT_LOW);
+	bool    up   = state->data->pose.vel > 0 && LIFT_LT(pos, POT_LIFT_HIGH);
+	bool    down = state->data->pose.vel < 0 && LIFT_GT(pos, POT_LIFT_LOW);
 	
 	return !(up || down);
 }
 
 #elif defined(ROBOT_NITISH)
 
-void auto_ramp_init(data_t *data) {
+void auto_ramp_init(state_t const __rom *state) {
 }
 
-void auto_ramp_loop(data_t *data) {
-	int8_t  vel   = data->pose.vel;
+void auto_ramp_loop(state_t const __rom *state) {
+	int8_t  vel   = state->data->pose.vel;
 	int16_t left  = analog_adc_get(POT_LIFT_L);
 	int16_t right = analog_adc_get(POT_LIFT_R);
 
@@ -116,8 +135,8 @@ void auto_ramp_loop(data_t *data) {
 	ramp_raw(move_left * vel, move_right * vel);
 }
 
-bool auto_ramp_isdone(data_t *data) {
-	int8_t  vel   = data->pose.vel;
+bool auto_ramp_isdone(state_t const __rom *state) {
+	int8_t  vel   = state->data->pose.vel;
 	int16_t left  = analog_adc_get(POT_LIFT_L);
 	int16_t right = analog_adc_get(POT_LIFT_R);
 
@@ -131,17 +150,23 @@ bool auto_ramp_isdone(data_t *data) {
 
 #endif
 
-bool auto_ram_isdone(data_t *data) {
+bool auto_ram_isdone(state_t const __rom *state) {
 	return !digital_get(BUT_B);
 }
 
 /* Do nothing for a given timeout.  */
-void auto_none_init(data_t *data) {
+void auto_none_init(state_t const __rom *state) {
 }
 
-void auto_none_loop(data_t *data) {
+void auto_none_loop(state_t const __rom *state) {
 }
 
-bool auto_none_isdone(data_t *data) {
-	return false;
+bool auto_none_isdone(state_t const __rom *state) {
+	/* TODO Replace this hack with a more accurate hack. */
+
+#if DEBUG
+	printf((char *)"timeout = %d\n\r", (int)state->data->timeout);
+#endif
+
+	return !state->timeout;
 }
--- comp2010/auton.h
+++ comp2010/auton.h
@@ -14,73 +14,73 @@
 
 #define SLOW_US 18500
 
-#define MS_TO_LOOPS(_x_) ((_x_) * (uint32_t)1000 / SLOW_US)
+#define MS_TO_LOOPS(_x_)   ((uint16_t)((_x_) * (uint32_t)1000 / SLOW_US))
+#define IN10_TO_TICKS(_x_) ((uint32_t)(_x_) * ENC_PER_IN / 10)
+#define DEG_TO_TICKS(_x_)  ((uint32_t)(_x_) * ENC_PER_DEG)
 
 /* Constructors used to wrap the initialization of a data_t. */
-#define AUTO_STRAIGHT(_timeout_, _dist_, _vel_) { MS_TO_LOOPS(_timeout_), (_dist_)* ENC_PER_IN,  (_vel_), 0, 0 }
-#define AUTO_RAM(_timeout_, _vel_)              { MS_TO_LOOPS(_timeout_), 0,                     (_vel_), 0, 0 }
-#define AUTO_TURN(_timeout_, _ang_, _vel_)      { MS_TO_LOOPS(_timeout_), (_ang_) * ENC_PER_DEG, (_vel_), 0, 0 }
-#define AUTO_ARM(_timeout_, _pos_, _vel_)       { MS_TO_LOOPS(_timeout_), (_pos_),               (_vel_), 0, 0 }
-#define AUTO_RAMP(_timeout_, _pos_, _vel_)      { MS_TO_LOOPS(_timeout_), (_pos_),               (_vel_), 0, 0 }
-#define AUTO_WAIT(_timeout_)                    { MS_TO_LOOPS(_timeout_), 0,                     0,       0, 0 }
+#define AUTO_STRAIGHT(_dist_, _vel_) { IN10_TO_TICKS(_dist_), (_vel_),         0, 0 }
+#define AUTO_DEPLOY()                { 0,                     LIFT_DEPLOY_VEL, 0, 0 }
+#define AUTO_RAM(_vel_)              { 0,                     (_vel_),         0, 0 }
+#define AUTO_TURN(_ang_, _vel_)      { DEG_TO_TICKS(_ang_),   (_vel_),         0, 0 }
+#define AUTO_ARM(_pos_, _vel_)       { (_pos_),               (_vel_),         0, 0 }
+#define AUTO_RAMP( _pos_, _vel_)     { (_pos_),               (_vel_),         0, 0 }
+#define AUTO_WAIT()                  { 0,                     0,               0, 0 }
 
 /* Define a transition function with name _name_ that transitions into the
  * _ntime_ state if auto_istimeout() is true, _ncond_ if _cond_ evaluates to
  * true, otherwise the current state.
  */
-#define STATE_START()                      \
-enum { _st_start = __LINE__ };             \
-extern state_t const __rom *auto_states[];
+#define STATE_START(_name_)    \
+enum { _st_start = __LINE__ }; \
+extern state_t const __rom _st_##_name_##_state; \
+state_t const __rom *auto_current = &_st_##_name_##_state;
 
 #define STATE_DONE() \
-enum { _st_end = __LINE__ }; \
-state_t const __rom *auto_states[STATE_NUM];
-
+enum { _st_num = __LINE__ - _st_start - 1 }; \
+uint16_t const __rom auto_num = _st_num;
 
 #define STATE_LOOKUP(_name_) _st_##_name_##_transition
 #define STATE_GET(_name_)    auto_states[_st_##_name_##_id]
-#define STATE_NUM            (_st_end - _st_start - 1)
 
-#define STATE(_name_, _data_, _cbinit_, _cbloop_, _stsuc_, _stfail_, _cond_)  \
-extern uint16_t const __rom _st_##_stsuc_##_id;                               \
-extern uint16_t const __rom _st_##_stfail_##_id;                              \
-uint16_t const __rom  _st_##_name_##_id = __LINE__ - _st_start - 1;           \
-state_t const __rom * _st_##_name_##_transition(state_t const __rom *state) { \
-	static data_t              _st_##_name_##_data  = _data_;                 \
-	static state_t const __rom _st_##_name_##_state = {                       \
-		#_name_, &_st_##_name_##_data,                                        \
-		_cbinit_, _cbloop_, _st_##_name_##_transition                         \
-	};                                                                        \
-	if (!state) {                                                             \
-		return &_st_##_name_##_state; /* For initialization code. */          \
-	} else if (!state->data->timeout) {                                       \
-		return STATE_GET(_stfail_);   /* Timeout condition. */                \
-	} else if (_cond_) {                                                      \
-		return STATE_GET(_stsuc_);    /* Success condition. */                \
+#define STATE(_name_, _timeout_, _data_, _cbinit_, _cbloop_, _stsuc_,         \
+              _stfail_, _cond_)                                               \
+/* Forward declare the states that are transitioned into. */                  \
+extern state_t const __rom _st_##_stsuc_##_state;                             \
+extern state_t const __rom _st_##_stfail_##_state;                            \
+state_t const __rom *_st_##_name_##_transition(state_t const __rom *);        \
+                                                                              \
+/* State structure and data used by this state. */                            \
+data_t              _st_##_name_##_data  = _data_;                            \
+state_t const __rom _st_##_name_##_state = {                                  \
+	MS_TO_LOOPS(_timeout_), #_name_, &_st_##_name_##_data,                    \
+	_cbinit_, _cbloop_, _st_##_name_##_transition                             \
+};                                                                            \
+                                                                              \
+state_t const __rom *_st_##_name_##_transition(state_t const __rom *state) {  \                                                                     \
+	if (state->timeout < 0) {                                                 \
+		return &_st_##_stfail_##_state; /* Timeout condition. */              \
+	} else if (_cond_(state)) {                                               \
+		return &_st_##_stsuc_##_state;  /* Success condition. */              \
 	} else {                                                                  \
-		return state;                 /* No transition. */                    \
+		return state;                   /* No transition. */                  \
 	}                                                                         \
 }
 
 typedef union {
 	/* Hack to enable shorthand union initalization. */
 	struct {
-		uint16_t unnamed1;
 		uint16_t unnamed2;
 		int8_t   unnamed3;
 		int32_t  unnamed4;
 		int32_t  unnamed5;
 	} unused;
 
-	/* Simulate polymorphism using clever struct alignment. */
-	uint16_t timeout;
-
 	/* Control the robot's position and orientation using a timeout and encoder
 	 * ticks. Applies to both forward movement and turning, although encoder
 	 * tick calculations are performed slightly differently.
 	 */
 	struct {
-		uint16_t timeout;
 		uint16_t ticks;
 		int8_t   vel;
 		int32_t  enc_left;
@@ -91,7 +91,6 @@ typedef union {
 	 * potentiometer value and a timeout.
 	 */
 	struct {
-		uint16_t timeout;
 		uint16_t pos;     /* potentiometer value */
 		int8_t   vel;
 	} pose;
@@ -101,13 +100,14 @@ typedef union {
 typedef struct state_s state_t;
 
 /* Generalized callback that does not trigger a state transition. */
-typedef void (*callback_t)(data_t *);
+typedef void (*callback_t)(state_t const __rom *);
 
 /* Callback specifically for transitioning states. */
 typedef state_t const __rom *(*transition_t)(state_t const __rom *);
 
 /* All necessary information to execute and transition from a state. */
 struct state_s {
+	int32_t           timeout;
 	char const __rom *name;
 	data_t      *     data;
 	callback_t        cb_init;
@@ -115,38 +115,34 @@ struct state_s {
 	transition_t      cb_next;
 };
 
-/* Initialize the data field for a state_t. */
-data_t auto_straight_create(uint16_t, uint16_t, int8_t);
-data_t auto_turn_create(uint16_t, uint16_t, int8_t);
-data_t auto_ram_create(uint16_t, int8_t);
-data_t auto_arm_create(uint16_t, uint16_t, int8_t);
-data_t auto_ramp_create(uint16_t, uint16_t, int8_t);
+void auto_deploy_init(state_t const __rom *);
+void auto_deploy_loop(state_t const __rom *);
 
 /* Drive straight for the specified distance.*/
-void auto_straight_init(data_t *);
-void auto_straight_loop(data_t *);
-bool auto_straight_isdone(data_t *);
+void auto_straight_init(state_t const __rom *);
+void auto_straight_loop(state_t const __rom *);
+bool auto_straight_isdone(state_t const __rom *);
 
 /* Turn through the specified number of degrees. */
-void auto_turn_init(data_t *);
-void auto_turn_loop(data_t *);
-bool auto_turn_isdone(data_t *);
+void auto_turn_init(state_t const __rom *);
+void auto_turn_loop(state_t const __rom *);
+bool auto_turn_isdone(state_t const __rom *);
 
 /* Rotate the arm into the specified position. */
-void auto_arm_init(data_t *);
-void auto_arm_loop(data_t *);
-bool auto_arm_isdone(data_t *);
+void auto_arm_init(state_t const __rom *);
+void auto_arm_loop(state_t const __rom *);
+bool auto_arm_isdone(state_t const __rom *);
 
 /* Raise or lower the ramp into the specified position. */
-void auto_ramp_init(data_t *);
-void auto_ramp_loop(data_t *);
-bool auto_ramp_isdone(data_t *);
+void auto_ramp_init(state_t const __rom *);
+void auto_ramp_loop(state_t const __rom *);
+bool auto_ramp_isdone(state_t const __rom *);
 
 /* Do nothing until the state times out. */
-void auto_none_init(data_t *);
-void auto_none_loop(data_t *);
-bool auto_none_isdone(data_t *);
+void auto_none_init(state_t const __rom *);
+void auto_none_loop(state_t const __rom *);
+bool auto_none_isdone(state_t const __rom *);
 
-bool auto_ram_isdone(data_t *);
+bool auto_ram_isdone(state_t const __rom *);
 
 #endif
