5fa9b9a6fb44a5272d9e30531ed75fbb3a8eb152
Michael Koval <koval.michael@gmail.com>
2010-04-20 19:25:18 -0400

    Fixed the vex-cortex Makefiles to successfully build Hax.

--- vex_cortex/gcc.mk
+++ vex_cortex/gcc.mk
@@ -1,17 +1,14 @@
-PREFIX        = arm-none-eabi-
-CC            = $(PREFIX)gcc
-LD            = $(PREFIX)gcc
-AS            = $(PREFIX)gcc
-MD            = $(PREFIX)gcc
-OBJCOPY       = $(PREFIX)objcopy
-OBJDUMP       = $(PREFIX)objdump
-STRIP         = $(PREFIX)strip
+ARCH_PREFIX   = arm-none-eabi-
+CC            = $(ARCH_PREFIX)gcc
+LD            = $(ARCH_PREFIX)gcc
+AS            = $(ARCH_PREFIX)gcc
+MD            = $(ARCH_PREFIX)gcc
+OBJCOPY       = $(ARCH_PREFIX)objcopy
+OBJDUMP       = $(ARCH_PREFIX)objdump
+STRIP         = $(ARCH_PREFIX)strip
+FIND          = find
 
 # External libraries.
-FWLIB_DIR   = $(srcdir)/lib/fwlib
-SRC_STM_LIB = $(srcdir)/lib/startup/startup_stm32f10x_hd.s \
-              $(wildcard $(FWLIB_DIR)/src/*.c)
-SRC_PRINTF  = $(wildcard $(srcdir)/lib/small_printf/*.c)
 CC_INC      = -I$(srcdir) -I$(ARCH)/lib/fwlib/inc -I$(ARCH)/lib
 LD_INC      = -L$(ARCH)/lib -L$(ARCH)/ld -L$(ARCH)/ld/other
 
@@ -33,38 +30,47 @@ ARCH_LDFLAGS=$(ALL_CFLAGS)                            \
             -fwhole-program -Wl,-static               \
             $(LD_INC) -T $(LD_SCRIPT)
 
-OBJECTS      += $(SOURCE:=.o)
-TRASH        += $(TARGET) $(OBJECTS) $(OBJECTS:.o.=.d) \
-                $(shell $(FIND) -E . -regex '.*\.([od]|elf|hex|bin|map|lss|sym|strip)' -delete)
+OBJECTS       = $(SOURCE:=.o)
+TRASH        += $(TARGET) $(OBJECTS) $(OBJECTS:.o.=.d)
+
+
+clean :
+	@$(RM) $(TRASH)
+	@$(FIND) -E . -regex '.*\.([od]|elf|hex|bin|map|lss|sym|strip)' -delete
+	@echo "CLEAN"
+
+rebuild : clean all
 
 .SECONDARY : 
 
 %.s.o: %.s $(HEADER)
-	$(AS) $(ALL_ASFLAGS) -c -o $@ $<
+	@$(AS) $(ALL_ASFLAGS) -c -o $@ $<
+	@echo "AS $<"
 
 %.c.o: %.c $(HEADER)
-	$(CC) $(ALL_CFLAGS) -c -o $@ $<
+	@$(CC) $(ALL_CFLAGS) -c -o $@ $<
+	@echo "CC $<"
 
 %.elf: $(OBJECTS)
-	$(LD) $(ALL_LDFLAGS) -o $@ $^
+	@$(LD) $(ALL_LDFLAGS) -o $@ $^
+	@echo "LD $<"
 
 %.hex: %.elf
-	$(OBJCOPY) -S -O ihex $< $@
+	@$(OBJCOPY) -S -O ihex $< $@
+	@echo "HEX $<"
 
 %.bin: %.elf
-	$(OBJCOPY) -S -O binary $< $@
+	@$(OBJCOPY) -S -O binary $< $@
+	@echo "BIN $<"
 
 # Create extended listing file from ELF output file.
 %.elf.lss: %.elf
-	$(OBJDUMP) -h -S $< > $@
+	@$(OBJDUMP) -h -S $< > $@
+	@echo "LSS $<"
 
 # Create a symbol table from ELF output file.
 %.elf.sym: %.elf
-	$(NM) -n $< > $@
-
-%.elf.strip: %.elf
-	$(STRIP) -S $< -o $@
-	$(NM) -n $<.strip > $@.sym
-	$(OBJDUMP) -h -S $<.strip > $@.lss
+	@$(NM) -n $< > $@
+	@echo "SYM $<"
 
 .PHONY : all clean install rebuild
