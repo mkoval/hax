5418e2f3c44a2d15fcb5a019096d7f4d75edbfd0
Michael Koval <koval.michael@gmail.com>
2010-04-20 17:24:02 -0400

    Ported the vex_cortex project into Hax. Still lacking interrupts and digital IO. Also was not able to get Cody's horribly complex Makefile to work.

--- vex_cortex/gcc.mk
+++ vex_cortex/gcc.mk
@@ -4,49 +4,67 @@ LD            = $(PREFIX)-gcc
 AS            = $(PREFIX)-gcc
 MD            = $(PREFIX)-gcc
 OBJCOPY       = $(PREFIX)-objcopy
+OBJDUMP       = $(PREFIX)objdump
+OBJCOPY       = $(REFIX)objcopy
+STRIP         = $(PREFIX)strip
 
-ARCH_CFLAGS  += -mcpu=cortex-m3 -D_STM32F103VDH6_ -D_STM3x_ -D_STM32x_     \
-                -mthumb -fsigned-char -ffunction-sections -Wall -Wno-main  \
-                -I$(FWLIB_PATH)/inc -I$(srcdir) -D_GPIOG
-ARCH_LDFLAGS += $(ARCH_CFLAGS) -Wl,-L$(LDSCRIPT_PATH) -Wl,-T$(LD_SCRIPT)   \
-                -Wl,-static,--gc-sections -nostartfiles                    \
-                -l:$(FWLIB_PATH)/lib/STM32F10x_thumb.lib
-ARCH_MDFLAGS += $(ARCH_CFLAGS)
+# External libraries.
+FWLIB_DIR   = $(srcdir)/lib/fwlib
+SRC_STM_LIB = $(srcdir)/lib/startup/startup_stm32f10x_hd.s \
+              $(wildcard $(FWLIB_DIR)/src/*.c)
+SRC_PRINTF  = $(wildcard $(srcdir)/lib/small_printf/*.c)
+CC_INC      = -I$(srcdir) -I$(srcdir)/lib/fwlib/inc -I$(srcdir)/lib
+LD_INC      = -L$(srcdir)/lib -L$(srcdir)/ld -L$(srcdir)/ld/other
 
-TARGET_ELF    = $(TARGET:.hex=.elf)
+LD_SCRIPT = STM32F103_384K_64K_FLASH.ld
+STMPROC   = STM32F10X_HD
+HSE_VALUE = 8000000
+
+ARCH_CFLAGS=-MD -D$(STMPROC) -DHSE_VALUE=$(HSE_VALUE) \
+           -mthumb -mcpu=cortex-m3 -Wall              \
+           -Wno-main -DUSE_STDPERIPH_DRIVER -pipe     \
+           -ffunction-sections -fno-unwind-tables     \
+           -D_SMALL_PRINTF -DNO_FLOATING_POINT        \
+           $(CC_INC) $(CFLAGS)
+
+ARCH_LDFLAGS=$(ALL_CFLAGS)                            \
+            -nostartfiles                             \
+            -Wl,--gc-sections,-Map=$@.map,-cref       \
+            -Wl,-u,Reset_Handler                      \
+            -fwhole-program -Wl,-static               \
+            $(LD_INC) -T $(LD_SCRIPT)
+
 OBJECTS      += $(SOURCE:=.o)
-TRASH        += $(OBJECTS) $(OBJECTS:.o.=.d)
+TRASH        += $(OBJECTS) $(OBJECTS:.o.=.d)          \
+                $(shell $(FIND) -E . -regex '.*\.([od]|elf|hex|bin|map|lss|sym|strip)' -delete)
 
 .SECONDARY : 
-.SUFFIXES:
 
-rebuild : clean all
+%.s.o: %.s $(HEADER)
+	$(AS) $(ALL_ASFLAGS) -c -o $@ $<
 
-clean :
-	@echo "CLEAN"
-	@$(RM) $(OBJECTS) $(TARGET) $(TARGET_ELF) $(TRASH)
+%.c.o: %.c $(HEADER)
+	$(CC) $(ALL_CFLAGS) -c -o $@ $<
 
-$(TARGET_ELF) : $(OBJECTS)
-	@echo "LD $(@F)"
-	@$(LD) -o $@ $^ $(ALL_LDFLAGS) 
+%.elf: $(OBJ)
+	$(LD) $(ALL_LDFLAGS) -o $@ $^
 
-%.hex : %.elf
-	@echo "HEX $(@F)"
-	@$(OBJCOPY) --target=ihex $< $@ 
+%.hex: %.elf
+	$(OBJCOPY) -S -O ihex $< $@
 
--include $(OBJECTS:.o=.d)
+%.bin: %.elf
+	$(OBJCOPY) -S -O binary $< $@
 
-%.c.o : %.c 
-	@echo "CC $(@F)"
-	@$(CC) $(ALL_CFLAGS) -c -o $@ $<
+# Create extended listing file from ELF output file.
+%.elf.lss: %.elf
+	$(OBJDUMP) -h -S $< > $@
+# Create a symbol table from ELF output file.
+%.elf.sym: %.elf
+	$(NM) -n $< > $@
 
-	@$(MD) $(ALL_MDFLAGS) -M -o $<.d $<
-	@mv -f $<.d $<.tmp
-	@sed -e 's|.*:|$<.o:|' < $<.tmp > $<.d
-	@$(RM) $<.tmp
-
-%.s.o : %.s $(HEADERS)
-	@echo "AS $(@F)"
-	@$(AS) $(ALL_ASFLAGS) -c -o $@ $<
+%.elf.strip: %.elf
+	$(STRIP) -S $< -o $@
+	$(NM) -n $<.strip > $@.sym
+	$(OBJDUMP) -h -S $<.strip > $@.lss
 
 .PHONY : all clean install rebuild
