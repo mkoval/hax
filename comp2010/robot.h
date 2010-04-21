d18ea3307954c749f4a4f44dfab501ef1953a3fd
Michael Koval <koval.michael@gmail.com>
2010-04-20 22:49:21 -0400

    Removed an invisible invalid ASCII character that looks like whitespace.

--- comp2010/robot.h
+++ comp2010/robot.h
@@ -6,7 +6,7 @@
 
 #define ARM_SET_ERRMAX        10
 #define DRIVE_STRAIGHT_ERRMAX 100
-#define DRIVE_TURN_ERRMAX     ((int16_t)30 * ENC_PER_DEG) 
+#define DRIVE_TURN_ERRMAX     ((int16_t)30 * ENC_PER_DEG)
 #define DRIVE_TURN_ERRMIN     ((int16_t)5  * ENC_PER_DEG)
 
 /* Control the speed of the drive motors to get a desired forward movement
