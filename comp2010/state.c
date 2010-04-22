8e8ce02bee5a4cb09a55e2e16041523d46a97d81
Michael Koval <koval.michael@gmail.com>
2010-04-22 09:27:05 -0500

    Duplicated the Nitish's autonomous mode as a starting point for the Kevin.

--- comp2010/state.c
+++ comp2010/state.c
@@ -7,6 +7,36 @@
 #if defined(ROBOT_KEVIN)
 
 STATE_START(start)
+STATE(start,      1,    AUTO_WAIT(),             auto_none_init,     auto_none_loop,     deploy,     done,       auto_none_isdone)
+STATE(deploy,     150,  AUTO_DEPLOY(),           auto_deploy_init,   auto_deploy_loop,   init_ram,   done,       auto_none_isdone)
+STATE(init_ram,   9000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, init_arm,   done,       auto_ram_isdone)
+STATE(init_arm,   5000, AUTO_ARM(0, -127),       auto_arm_init,      auto_arm_loop,      init_back,  done,       auto_none_isdone)
+STATE(init_back,  9000, AUTO_STRAIGHT(30, -127), auto_straight_init, auto_straight_loop, init_dump1, init_dump1, auto_straight_isdone)
+STATE(init_dump1, 5000, AUTO_RAMP(100, 127),     auto_ramp_init,     auto_ramp_loop,     init_dump2, init_dump2, auto_ramp_isdone)
+STATE(init_dump2, 5000, AUTO_WAIT(),             auto_none_init,     auto_none_loop,     init_dump3, done,       auto_none_isdone)
+
+/* Collect the white ball. */
+STATE(init_dump3, 5000, AUTO_RAMP(0, -127),      auto_ramp_init,     auto_ramp_loop,     big_turn,   big_turn,  auto_ramp_isdone)
+STATE(big_turn,   2000, AUTO_TURN(40, -127),     auto_turn_init,     auto_turn_loop,     big_drive,  done,      auto_turn_isdone)
+STATE(big_drive,  9000, AUTO_STRAIGHT(500,-127), auto_straight_init, auto_straight_loop, big_jig1,   done,      auto_straight_isdone)
+STATE(big_jig1,   2000, AUTO_TURN(25, 127),      auto_turn_init,     auto_turn_loop,     big_jig2,   big_jig2,  auto_turn_isdone)
+STATE(big_jig2,   500,  AUTO_ARM(100, 127),      auto_arm_init,      auto_arm_loop,      big_ram,    done,      auto_none_isdone)
+
+/* Dump the previously harvested white ball. */
+STATE(big_ram,    9000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, big_arm1,   big_arm2,  auto_ram_isdone)
+STATE(big_arm1,   4000, AUTO_ARM(100, 127),      auto_arm_init,      auto_arm_loop,      big_arm2,   done,      auto_arm_isdone)
+STATE(big_arm2,   4000, AUTO_ARM(0, -127),       auto_arm_init,      auto_arm_loop,      big_back,   done,      auto_arm_isdone)
+STATE(big_back,   5000, AUTO_STRAIGHT(30, -127), auto_straight_init, auto_straight_loop, big_dump1,  done,      auto_straight_isdone)
+STATE(big_dump1,  5000, AUTO_RAMP(100, 127),     auto_ramp_init,     auto_ramp_loop,     big_dump2,  big_dump2, auto_ramp_isdone)
+STATE(big_dump2,  5000, AUTO_WAIT(),             auto_none_init,     auto_none_loop,     big_dump3,  big_dump3, auto_none_isdone)
+STATE(big_dump3,  5000, AUTO_RAMP(0, -127),      auto_ramp_init,     auto_ramp_loop,     big_catch,  big_catch, auto_ramp_isdone)
+
+/* Prepare for telop. */
+STATE(big_catch,  5000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, done,       done,      auto_ram_isdone)
+STATE(done,       1,    AUTO_WAIT(),             auto_none_init,     auto_none_loop,     done,       done,      auto_none_isdone)
+
+
+#if 0
 /* Deploy the arm from its starting ("up") position. */
 STATE(start,       1,    AUTO_WAIT(),             auto_none_init,     auto_none_loop,     deploy,      deploy, auto_none_isdone)
 STATE(deploy,      150,  AUTO_DEPLOY(),           auto_deploy_init,   auto_deploy_loop,   init_ram,    done,   auto_none_isdone)
@@ -30,6 +60,7 @@ STATE(dep_dump1,   2000, AUTO_RAMP(100, 127),     auto_ramp_init,     auto_ramp_
 STATE(dep_dump2,   5000, AUTO_WAIT(),             auto_none_init,     auto_none_loop,     dep_dump3,   done,   auto_none_isdone)
 STATE(dep_dump3,   2000, AUTO_RAMP(0, 127),       auto_ramp_init,     auto_ramp_loop,     done,        done,   auto_ramp_isdone)
 STATE(done,        1,    AUTO_WAIT(),             auto_none_init,     auto_none_loop,     done,        done,   auto_none_isdone)
+#endif
 
 #elif defined(ROBOT_NITISH)
 
