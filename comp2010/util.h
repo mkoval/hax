8f90177ce63ead077f9e4b807b336dc94ee631e0
Michael Koval <koval.michael@gmail.com>
2010-04-22 08:44:33 -0500

    Constrain the OI inputs to avoid overflow when driving.

--- comp2010/util.h
+++ comp2010/util.h
@@ -6,5 +6,6 @@
 #define MAX(_x_, _y_) (((_x_) > (_y_)) ? (_x_) : (_y_))
 #define SIGN(_x_)     (((_x_) < 0) ? -1 : 1)
 #define PROP(_mout_, _merr_, _err_) ((_mout_)*MIN((_err_), (_merr_)) / (_merr_))
+#define CONSTRAIN(_x_, _min_, _max_) MAX(MIN(_x_, _max_), _min_)
 
 #endif
