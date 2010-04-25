44dede4b7255e070a16808194c078349cad49523
Michael Koval <koval.michael@gmail.com>
2010-04-25 14:51:52 -0500

    Tweaked autonomous for the last time. Still doesn't reliably pick up the white ball.

--- comp2010/state.c
+++ comp2010/state.c
@@ -16,12 +16,10 @@ STATE(init_dump2, 4000, AUTO_WAIT(),             auto_none_init,     auto_none_l
 
 /* Collect the white ball. */
 STATE(init_dump3, 4000, AUTO_RAMP(0, -127),      auto_ramp_init,     auto_ramp_loop,     big_turn,   big_turn,   auto_ramp_isdone)
-STATE(big_turn,   500,  AUTO_TURN(9, 127),       auto_turn_init,     auto_turn_loop,     big_jerk,   big_jerk,   auto_turn_isdone)
-STATE(big_jerk,   350,  AUTO_ARM(100, 127),      auto_arm_init,      auto_arm_loop,      big_fall,   big_fall,   auto_none_isdone)
-STATE(big_fall,   2000, AUTO_WAIT(),             auto_none_init,     auto_none_init,     big_drive,  big_drive,  auto_none_isdone)
+STATE(big_turn,   500,  AUTO_TURN(9, 127),       auto_turn_init,     auto_turn_loop,     big_drive,  big_drive,  auto_turn_isdone)
 STATE(big_drive,  6000, AUTO_STRAIGHT(500,-127), auto_straight_init, auto_straight_loop, big_jig,    big_jig,    auto_straight_isdone)
 STATE(big_jig,    2000, AUTO_TURN(9, -127),      auto_turn_init,     auto_turn_loop,     big_shove,  big_shove,  auto_turn_isdone)
-STATE(big_shove,  600,  AUTO_STRAIGHT(50, -127), auto_straight_init, auto_turn_init,     big_pickup, big_pickup, auto_straight_isdone)
+STATE(big_shove,  1000, AUTO_DRIVE(-127),        auto_drive_init,    auto_drive_loop,    big_pickup, big_pickup, auto_none_isdone)
 STATE(big_pickup, 5000, AUTO_PICKUP(70),         auto_pickup_init,   auto_pickup_loop,   big_ram,    big_ram,    auto_pickup_isdone)
 
 /* Dump the previously harvested white ball. */
@@ -40,25 +38,25 @@ STATE(done,       1,    AUTO_WAIT(),             auto_none_init,     auto_none_l
 STATE_START(start)
 /* Dump the preloaded balls. */
 STATE(start,      1,    AUTO_WAIT(),             auto_none_init,     auto_none_loop,     init_ram,   done,       auto_none_isdone)
-STATE(init_ram,   8000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, init_back,  init_arm,   auto_ram_isdone)
-STATE(init_back,  3000, AUTO_STRAIGHT(25, -127), auto_straight_init, auto_straight_loop, init_arm,   init_arm,   auto_straight_isdone)
-STATE(init_arm,   5000, AUTO_ARM(0, -127),       auto_arm_init,      auto_arm_loop,      init_dump1, done,       auto_none_isdone)
+STATE(init_ram,   8000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, init_arm,   init_arm,   auto_ram_isdone)
+STATE(init_arm,   5000, AUTO_ARM(0, -127),       auto_arm_init,      auto_arm_loop,      init_back,  init_back,  auto_none_isdone)
+STATE(init_back,  3000, AUTO_STRAIGHT(20, -127), auto_straight_init, auto_straight_loop, init_dump1, init_dump1, auto_straight_isdone)
 STATE(init_dump1, 5000, AUTO_RAMP(100, 127),     auto_ramp_init,     auto_ramp_loop,     init_dump2, init_dump2, auto_ramp_isdone)
 STATE(init_dump2, 5000, AUTO_WAIT(),             auto_none_init,     auto_none_loop,     init_dump3, done,       auto_none_isdone)
 
 /* Collect the white ball. */
 STATE(init_dump3, 5000, AUTO_RAMP(0, -127),      auto_ramp_init,     auto_ramp_loop,     big_turn,   big_turn,   auto_ramp_isdone)
-STATE(big_turn,   2000, AUTO_TURN(22, -127),     auto_turn_init,     auto_turn_loop,     big_jerk,   big_jerk,   auto_turn_isdone)
+STATE(big_turn,   2000, AUTO_TURN(22, -127),     auto_turn_init,     auto_turn_loop,     big_drive,  big_drive,  auto_turn_isdone)
 /* Prevent the arm from digging into the foam mat. */
-STATE(big_jerk,   800,  AUTO_ARM(0, 127),        auto_arm_init,      auto_arm_loop,      big_drive,  big_drive,  auto_none_isdone)
 STATE(big_drive,  8000, AUTO_STRAIGHT(500,-127), auto_straight_init, auto_straight_loop, big_jig,    big_jig,    auto_straight_isdone)
-STATE(big_jig,    2000, AUTO_TURN(15, 127),      auto_turn_init,     auto_turn_loop,     big_pickup, big_pickup, auto_turn_isdone)
-STATE(big_pickup, 5000, AUTO_PICKUP(80),        auto_pickup_init,   auto_pickup_loop,   big_ram,    big_ram,    auto_pickup_isdone)
+STATE(big_jig,    2000, AUTO_TURN(15, 127),      auto_turn_init,     auto_turn_loop,     big_shove,  big_shove,  auto_turn_isdone)
+STATE(big_shove,  1000, AUTO_DRIVE(-127),        auto_drive_init,    auto_drive_loop,    big_pickup, big_pickup, auto_none_isdone)
+STATE(big_pickup, 5000, AUTO_PICKUP(80),         auto_pickup_init,   auto_pickup_loop,   big_ram,    big_ram,    auto_pickup_isdone)
 
 /* Dump the previously harvested white ball. */
-STATE(big_ram,    9000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, big_back,   big_arm,   auto_ram_isdone)
-STATE(big_back,   8000, AUTO_STRAIGHT(25, -127), auto_straight_init, auto_straight_loop, big_arm,    big_arm,   auto_straight_isdone)
-STATE(big_arm,    4000, AUTO_ARM(0, -127),       auto_arm_init,      auto_arm_loop,      big_dump1,  done,      auto_arm_isdone)
+STATE(big_ram,    9000, AUTO_RAM(127),           auto_straight_init, auto_straight_loop, big_arm,    big_arm,   auto_ram_isdone)
+STATE(big_arm,    4000, AUTO_ARM(0, -127),       auto_arm_init,      auto_arm_loop,      big_back,   big_back,  auto_arm_isdone)
+STATE(big_back,   8000, AUTO_STRAIGHT(20, -127), auto_straight_init, auto_straight_loop, big_dump1,  big_dump1, auto_straight_isdone)
 STATE(big_dump1,  5000, AUTO_RAMP(100, 127),     auto_ramp_init,     auto_ramp_loop,     big_dump2,  big_dump2, auto_ramp_isdone)
 STATE(big_dump2,  5000, AUTO_WAIT(),             auto_none_init,     auto_none_loop,     big_dump3,  big_dump3, auto_none_isdone)
 STATE(big_dump3,  5000, AUTO_RAMP(0, -127),      auto_ramp_init,     auto_ramp_loop,     big_catch,  big_catch, auto_ramp_isdone)
