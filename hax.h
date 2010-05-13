a75cde1efc0fece0c321850a7877515b8b624e7e
Michael Koval <koval.michael@gmail.com>
2010-05-12 23:37:38 -0400

    Fixed the type of isr_t to accept a bool (usually a uint8_t) instead of an int8_t.

--- hax.h
+++ hax.h
@@ -14,7 +14,7 @@ void main(void) __noreturn;
 typedef uint8_t index_t;
 
 /* Callback function invoked when a hardware interrupt is fired. */
-typedef void (*isr_t)(int8_t pin_level);
+typedef void (*isr_t)(bool pin_level);
 
 /* Operating mode of the robot (autonomous or operator controlled). */
 typedef enum {
