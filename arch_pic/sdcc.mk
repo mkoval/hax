3287b25cf00d8451e53c176863555bce26caa736
jmesmon <jmesmon@gmail.com>
2010-05-23 03:35:46 -0400

    beginnings of sdcc support, writing sdcc.mk to replace mcc18.mk

---
+++ arch_pic/sdcc.mk
@@ -0,0 +1,40 @@
+CC = sdcc
+#LD = gplink
+#AS = gpasm
+LD = sdcc
+AS = sdcc
+
+ARCH_CFLAGS = -mpic16 -p18f8520 
+ARCH_CFLAGS += -I$(srcdir) -I$(srcdir)/$(ARCH)
+ARCH_CFLAGS += --ivt-loc=0x800
+ARCH_CFLAGS += --optimize-cmp --optimize-goto
+#ARCH_CFLAGS += --optimize-df
+#ARCH_CFLAGS += --pstack-model=large
+
+ARCH_ASFLAGS = -p 18f8520
+
+OBJECTS     += $(SOURCE:=.o)
+#TRASH       += 
+
+.SUFFIXES:
+.SECONDARY:
+
+all: $(TARGET)
+
+clean :
+	@echo "CLEAN"
+	@$(RM) $(OBJECTS) $(TARGET) $(TRASH)
+
+$(TARGET) : $(OBJECTS)
+	@echo "LD $(@F)"
+	@$(LD) $(ALL_LDFLAGS) $^ $@
+
+%.c.o : %.c
+	@echo "CC $(@F)"
+	@$(CC) $(ALL_CFLAGS) -c -o $@ $<
+
+%.asm.o : %.asm
+	@echo "AS $(@F)"
+	@$(AS) $(ALL_ASFLAGS) -c -o $@ $<
+
+.PHONY: all clean
