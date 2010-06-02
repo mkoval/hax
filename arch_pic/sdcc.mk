6b4bd935bc1b248e9e87c7735b05441d52521c69
jmesmon <jmesmon@gmail.com>
2010-06-02 16:48:16 -0400

    pic sdcc: remove apparently invalid compiler flag

--- arch_pic/sdcc.mk
+++ arch_pic/sdcc.mk
@@ -9,7 +9,7 @@ ARCH_CFLAGS = -mpic16 -p18f8520
 ARCH_CFLAGS += -I$(srcdir) -I$(srcdir)/$(ARCH)
 ARCH_CLFAGS += -I$(srcdir)/$(ARCH)/include
 ARCH_CFLAGS += --ivt-loc=0x800 --no-crt
-ARCH_CFLAGS += --optimize-cmp --optimize-goto
+ARCH_CFLAGS += --optimize-cmp
 ARCH_CFLAGS += --optimize-df
 #ARCH_CFLAGS += --pstack-model=large
 
