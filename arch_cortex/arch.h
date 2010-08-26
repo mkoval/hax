f4e939640f1b83d629cef3e46ef018302728a925
Michael Koval <koval.michael@gmail.com>
2010-08-26 04:17:31 -0400

    Removed superflous checks from arch.h

--- arch_cortex/arch.h
+++ arch_cortex/arch.h
@@ -1,9 +1,9 @@
 #ifndef ARCH_CORTEX_H_
 #define ARCH_CORTEX_H_
 
-#define PIN_ANALOG(_x_)  ((1 <= (_x_) && (_x_) <= 8)  ? (_x_) + 0  : 0)
-#define PIN_DIGITAL(_x_) ((1 <= (_x_) && (_x_) <= 12) ? (_x_) + 8  : 0)
-#define PIN_MOTOR(_x_)   ((1 <= (_x_) && (_x_) <= 10) ? (_x_) + 20 : 0)
-#define PIN_OI(_x_)      ((1 <= (_x_) && (_x_) <= 8)  ? (_x_) + 30 : 0)
+#define PIN_ANALOG(_x_)  ((_x_) - 0)  /*  1 -  8 */
+#define PIN_DIGITAL(_x_) ((_x_) + 8)  /*  8 - 20 */
+#define PIN_MOTOR(_x_)   ((_x_) + 20) /* 21 - 31 */
+#define PIN_OI(_x_)      ((_x_) + 32) /* 32 - 40 */
 
 #endif
--- arch_pic/arch.h
+++ arch_pic/arch.h
@@ -1,9 +1,9 @@
 #ifndef ARCH_PIC_H_
 #define ARCH_PIC_H_
 
-#define PIN_DIGITAL(_x_)   ((1 <= (_x_) && (_x_) <= 16) ? (_x_) + 0  : 0)
-#define PIN_INTERRUPT(_x_) ((1 <= (_x_) && (_x_) <= 6)  ? (_x_) + 16 : 0)
-#define PIN_MOTOR(_x_)     ((1 <= (_x_) && (_x_) <= 8)  ? (_x_) + 22 : 0)
-#define PIN_OI(_x_)        ((1 <= (_x_) && (_x_) <= 6)  ? (_x_) + 30 : 0)
+#define PIN_DIGITAL(_x_)   ((_x_) + 0)  /*  1 - 16 */
+#define PIN_INTERRUPT(_x_) ((_x_) + 16) /* 17 - 23 */
+#define PIN_MOTOR(_x_)     ((_x_) + 22) /* 24 - 32 */
+#define PIN_OI(_x_)        ((_x_) + 41) /* 33 - 41 */
 
 #endif
