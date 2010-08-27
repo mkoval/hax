caff7b145881d66fb060132705190b9d8fa902a6
John Mesmon <jmesmon@gmail.com>
2010-08-27 01:58:39 -0400

    comp2010/encoder: partial update for new api + old fixes for brokeness

--- comp2010/encoder.c
+++ comp2010/encoder.c
@@ -4,33 +4,36 @@
 
 #include "encoder.h"
 
-static bool intenc[6] = { false };
+static bool intenc[6] = {};
 
 /* Mapping between logical "encoder index" and interrupt pins indexes. */
-static volatile InterruptIx encmap[6];
+static volatile index_t encmap[6];
 
 /* Store ticks in each direction in different memory locations to avoid
  * corruption from small oscillations.
  */
 static volatile uint32_t counts[6];
 
-#define ENCODER(_flip_,_other_,_num_)        \
+/* FIXME: digital_get is not necissarily reading the right pin, depending on
+ * mapping.
+ */
+#define ENCODER(_flip_, _other_, _num_)      \
     do {                                     \
-        bool other = interrupt_get(_other_); \
+        bool other = digital_get(_other_);   \
         if (_flip_) {                        \
             if (other)                       \
-                ++counts[2 *_num_ + 1];      \
+                ++counts[2 *(_num_) + 1];    \
             else                             \
-                ++counts[2 * _num_];         \
+                ++counts[2 *(_num_)];        \
         } else {                             \
             if (other)                       \
-                ++counts[2 * _num_];         \
+                ++counts[2 *(_num_)];        \
             else                             \
-                ++counts[2 * _num_ + 1];     \
+                ++counts[2 *(_num_) + 1];    \
         }                                    \
     } while (0)
 
-void encoder_init(uint8_t id, InterruptIx int1, InterruptIx int2) {
+void encoder_init(uint8_t id, index_t int1, index_t int2) {
 	encmap[2 * id + 0] = int1;
 	encmap[2 * id + 1] = int2;
 
@@ -38,26 +41,26 @@ void encoder_init(uint8_t id, InterruptIx int1, InterruptIx int2) {
 	intenc[2 * id + 0] = true;
 	intenc[2 * id + 1] = true;
 
-	/* Enable interrupts for both encoder connections. */
-	interrupt_enable(int1);
-	interrupt_enable(int2);
-
 	switch (id) {
 	case 0:
-		interrupt_reg_isr(0, encoder_0a);
-		interrupt_reg_isr(1, encoder_0b);
+		interrupt_init(int1, encoder_0a);
+		interrupt_init(int2, encoder_0b);
 		break;
 	
 	case 1:
-		interrupt_reg_isr(2, encoder_1a);
-		interrupt_reg_isr(3, encoder_1b);
+		interrupt_init(int1, encoder_1a);
+		interrupt_init(int2, encoder_1b);
 		break;
 	
 	case 2:
-		interrupt_reg_isr(4, encoder_2a);
-		interrupt_reg_isr(5, encoder_2b);
+		interrupt_init(int1, encoder_2a);
+		interrupt_init(int2, encoder_2b);
 		break;
 	}
+	
+	/* Enable interrupts for both encoder connections. */
+	interrupt_set(int1, true);
+	interrupt_set(int2, true);
 }
 
 void encoder_update(void) {
@@ -80,25 +83,25 @@ void encoder_reset_all(void) {
 }
 
 void encoder_0a(int8_t l) {
-	ENCODER(l, encmap[1], 0);
+	ENCODER(l, encmap[1], encmap[0]);
 }
 
 void encoder_0b(int8_t l) {
-	ENCODER(!l, encmap[0], 0);
+	ENCODER(!l, encmap[0], encmap[1]);
 }
 
 void encoder_1a(int8_t l) {
-	ENCODER(l, encmap[3], 1);
+	ENCODER(l, encmap[3], encmap[2]);
 }
 
 void encoder_1b(int8_t l) {
-	ENCODER(!l, encmap[2], 1);
+	ENCODER(!l, encmap[2], encmap[3]);
 }
 
 void encoder_2a(int8_t l) {
-	ENCODER(l, encmap[5], 2);
+	ENCODER(l, encmap[5], encmap[4]);
 }
 
 void encoder_2b(int8_t l) {
-	ENCODER(!l, encmap[4], 2);
+	ENCODER(!l, encmap[4], encmap[5]);
 }
