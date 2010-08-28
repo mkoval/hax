a15db323a33af9c67785efd9c4649e3e04bad5f2
Michael Koval <koval.michael@gmail.com>
2010-08-28 15:26:34 -0400

    Moved internal pin-type offsets to macros

--- arch_cortex/arch.h
+++ arch_cortex/arch.h
@@ -4,11 +4,17 @@
 /* Period of the slow loop, in microseconds. */
 #define SLOW_US 18200
 
+/* Offset for each block of contiguous pins. */
+#define OFFSET_ANALOG     0
+#define OFFSET_DIGITAL    8
+#define OFFSET_MOTOR     20
+#define OFFSET_OI        32
+
 /* Utility macros for generating internal pin indexes. */
-#define PIN_ANALOG(_x_)  ((_x_) - 0)  /*  1 -  8 */
-#define PIN_DIGITAL(_x_) ((_x_) + 8)  /*  8 - 20 */
-#define PIN_MOTOR(_x_)   ((_x_) + 20) /* 21 - 31 */
-#define PIN_OI(_x_)      ((_x_) + 32) /* 32 - 40 */
+#define PIN_ANALOG(_x_)  ((_x_) + OFFSET_ANALOG)
+#define PIN_DIGITAL(_x_) ((_x_) + OFFSET_DIGITAL)
+#define PIN_MOTOR(_x_)   ((_x_) + OFFSET_MOTOR)
+#define PIN_OI(_x_)      ((_x_) + OFFSET_OI)
 
 /* Check a single button press on one of the VEXNet joystick's button pads. */
 #define BUT_UP(_x_)      ((_x_) & 1 != 0)
