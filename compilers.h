90c2e254e8fd1c838e790937133564f4e390269e
John Mesmon <jmesmon@gmail.com>
2010-08-28 19:36:50 -0400

    compilers.h: cleanup.

--- compilers.h
+++ compilers.h
@@ -1,38 +1,23 @@
 #ifndef COMPILERS_H_
 #define COMPILERS_H_
 
-/* MCC18 does not support a __noreturn flag as per Page 104 of the MCC18
- * User Guide.
- */
 #if defined(__18CXX)
-  //FIXME: need to actually detect version.
-  #define MCC18 300
-
-  #define __noreturn 
-  #define __rom	rom
-  #define __overlay overlay
-
+//FIXME: need to actually detect version.
+# define MCC18 300
+# define __noreturn
+# define __rom	rom
+# define __overlay overlay
 #elif defined(__GNUC__)
-  #define GCC 1
-
-  //XXX: Depricated. ARCH and COMPILER are seperate.
-  #define GCC_ARM 1
-
-  #define __noreturn __attribute__((noreturn))
-  #define __rom
-  #define __overlay
-
+# define GCC 1
+# define __noreturn __attribute__((noreturn))
+# define __rom
+# define __overlay
 #elif defined(SDCC)
-  // SDCC is a nice define to check, lets use it.
-
-  #define __rom __code
-  #define __noreturn 
-  #define __overlay
-
+# define __rom __code
+# define __noreturn
+# define __overlay
 #else
-
- #error "Unsupported Compiler"
-
+# error "Unsupported Compiler"
 #endif
 
 #endif /* COMPILERS_H_ */
