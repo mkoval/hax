67ef5a0486cf9b01170e227b3e8ca9eef50903b9
Michael Koval <koval.michael@gmail.com>
2010-09-01 22:41:13 -0400

    Merged interrupt.c and interrupts.c

--- arch_cortex/interrupt.c
+++ arch_cortex/interrupt.c
@@ -1,8 +1,11 @@
 /*
  * INTERRUPTS
  */
-void interrupt_init(index_t pin, isr_t isr) {
+void interrupt_init(index_t pin, isr_t isr)
+{
+	
 }
 
-void interrupt_set(index_t pin, bool enable) {
+void interrupt_set(index_t pin, bool enable)
+{
 }
