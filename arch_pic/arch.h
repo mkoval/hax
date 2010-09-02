d104c43b5bd699a0aea1b90c3ddfdcf28cf2d090
John Mesmon <jmesmon@gmail.com>
2010-09-01 21:02:23 -0400

    pic: change how the PIN_ macro's function.

--- arch_pic/arch.h
+++ arch_pic/arch.h
@@ -4,26 +4,40 @@
 /* Slow loop of 18.5 milliseconds (converted to microseconds). */
 #define SLOW_US 18500
 
-#define JOY1_L_X PIN_OI(0)
-#define JOY1_L_Y PIN_OI(1)
-#define JOY1_R_X PIN_OI(2)
-#define JOY1_R_Y PIN_OI(3)
+/* Counts for IO 'things' */
+#define CT_DIGITAL    16
+#define CT_INTERRUPT   6
+#define CT_MOTOR       8
+#define CT_OIx         6
+#define CT_OI         (2 * CT_OIx)
+#define CT_OIx_BUTTON  8
+#define CT_OI_GROUP_SZ 2
+#define CT_OI_BUTTON  (2 * CT_OIx_BUTTON)
 
-#define JOY2_L_X PIN_OI(8)
-#define JOY2_L_Y PIN_OI(9)
-#define JOY2_R_X PIN_OI(10)
-#define JOY2_R_Y PIN_OI(11)
+/* PIN_OI_BUTTON dir arguments */
+#define OI_B_UP 0
+#define OI_B_DN 1
 
 /* Offset for each block of contiguous pins. */
 #define OFFSET_DIGITAL      0
-#define OFFSET_INTERRUPT   16
-#define OFFSET_MOTOR       22
-#define OFFSET_OI          41
+#define OFFSET_INTERRUPT   (OFFSET_DIGITAL + CT_DIGITAL)
+#define OFFSET_MOTOR       (OFFSET_INTERRUPT + CT_INTERRUPT)
+#define OFFSET_OI          (OFFSET_MOTOR + CT_MOTOR)
+#define OFFSET_OI_BUTTON   (OFFSET_OI + CT_OI)
 
 /* Utility macros for generating internal pin indexes. */
-#define PIN_DIGITAL(_x_)   ((_x_) + OFFSET_DIGITAL)
-#define PIN_INTERRUPT(_x_) ((_x_) + OFFSET_INTERRUPT)
-#define PIN_MOTOR(_x_)     ((_x_) + OFFSET_MOTOR)
-#define PIN_OI(_x_)        ((_x_) + OFFSET_OI)
+#define PIN_DIGITAL(pin)      ((pin) + OFFSET_DIGITAL)
+#define PIN_INTERRUPT(pin)    ((pin) + OFFSET_INTERRUPT)
+#define PIN_MOTOR(n)          ((n) + OFFSET_MOTOR)
+#define PIN_OI(oi, group)     (OFFSET_OI + ((oi) * CT_IOx) + (group))
+#define PIN_OI_BUTTON(io, group, dir) \
+	(OFFSET_OI_BUTTON + ((oi) * CT_IOx) + \
+	 ((group) * CT_OI_GROUP_SZ) + dir)
+
+/* Joysticks */
+#define JOY_L_X(oi) PIN_OI(oi, 0)
+#define JOY_L_Y(oi) PIN_OI(oi, 1)
+#define JOY_R_X(oi) PIN_OI(oi, 2)
+#define JOY_R_Y(oi) PIN_OI(oi, 3)
 
 #endif
