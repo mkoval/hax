6b102ff78808351ff051a08c3b9c7624b191a9f8
John Mesmon <jmesmon@gmail.com>
2010-09-06 11:08:52 -0400

    hax_main: remove whitespace errors.

--- hax_main.c
+++ hax_main.c
@@ -3,18 +3,18 @@
 
 __noreturn void main(void) {
 	static state_t mode;
-	
+
 	setup_1();
 	init();
 	setup_2();
-	
+
 	for(;;) {
 		mode = mode_get();
-		
+
 		/* The "slow loop", executes once every kSlowSpeed microseconds. */
 		if (new_data_received()) {
 			loop_1();
-			
+
 			switch (mode) {
 			case MODE_AUTON:
 				auton_loop();
@@ -30,7 +30,7 @@ __noreturn void main(void) {
 
 			loop_2();
 		}
-		
+
 		/* Executes as fast as the hardware allows. */
 		spin();
 
