494bb36adef8a525d92914922dfbd30bf179e94e
Michael Koval <koval.michael@gmail.com>
2010-09-07 01:26:06 -0400

    Removed the need for manually defining HAX_PATH

--- build.mk
+++ build.mk
@@ -3,9 +3,11 @@ ALL_LDFLAGS = $(ARCH_LDFLAGS) $(LDFLAGS)
 ALL_ASFLAGS = $(ARCH_ASFLAGS) $(ASFLAGS)
 ALL_MDFLAGS = $(ARCH_MDFLAGS) $(MDFLAGS)
 
-ARCH   = $(HAX_PATH)/arch_$(arch)
-TARGET = $(prog)-$(arch).$(ARCH_EXT)
-srcdir = $(HAX_PATH)
+# Use the path to this Makefile as the base directory of HAX.
+srcdir     := $(dir $(lastword $(MAKEFILE_LIST)))
+srcdir     := $(srcdir:/=)
+ARCH        = $(srcdir)/arch_$(arch)
+TARGET      = $(prog)-$(arch).$(ARCH_EXT)
 
 .SUFFIXES:
 .PHONY: all clean build rebuild
