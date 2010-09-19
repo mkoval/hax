9164d2723b1971b0a6163cdfacd2d826f35ae447
John Mesmon <jmesmon@gmail.com>
2010-09-18 23:47:17 -0400

    comp2010: try to get warnings.

--- comp2010/prog_conf.h
+++ comp2010/prog_conf.h
@@ -4,4 +4,16 @@
 #include <ports.h>
 #define PROG_ANALOG_IN_NUM ANA_NUM
 
+#define WARN(...) _WARN(__LINE__,__VA_ARGS__)
+
+#include <stdio.h>
+#define _WARN(line, ...) __WARN(line, __VA_ARGS__)
+#define __WARN(line, ...) do {                  \
+	fputs(__FILE__ ":" #line ":", stdout);  \
+	fputs(__FUNCTION__, stdout);            \
+	fputs("() : ", stdout);                 \
+	printf(__VA_ARGS__);                    \
+	putchar('\n');                          \
+} while(0)
+
 #endif
