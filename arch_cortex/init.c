5c7b7dc0f1a5519a4ccb302bc687facfc7f4a3cb
John Mesmon <jmesmon@gmail.com>
2011-01-14 17:56:40 -0500

    oeg: if WARN uses a function, it needs to decl it in prog_conf.h

--- arch_cortex/init.c
+++ arch_cortex/init.c
@@ -158,7 +158,7 @@ __attribute__((interrupt)) void TIM1_CC_IRQHandler(void)
 void assert_failed(u8* file, u32 line)
 {
 	/* User can add his own implementation to report the file name and line number,
-	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	 ex: printf("Wrong parameters value: file %s on line %d\n", file, line) */
 	printf("error %s:%d - assert failed\n", file, line);
 
 	/* Infinite loop */
--- oeg/prog_conf.h
+++ oeg/prog_conf.h
@@ -3,6 +3,8 @@
 
 /* This header is included by other hax source files to
  * determine how the uC should be configured */
+#include <stdio.h>
+
 
 #define PROG_ANALOG_IN_NUM 16
 #define WARN(fmt, ...) printf(fmt, ##__VA_ARGS__)
