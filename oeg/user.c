b8ddc343561cc51693943e525cc5f356e3b80588
Michael Koval <koval.michael@gmail.com>
2011-01-16 20:01:57 -0500

    oeg: added button_get() test code for group 5

--- oeg/user.c
+++ oeg/user.c
@@ -32,8 +32,9 @@ void telop_loop(void) {
 	int8_t oi_left  = oi_group_get(OI_JOY_L_Y(1));
 	int8_t oi_arm   = oi_rocker_get(OI_ROCKER_L(1));
 
+	printf("left, up   = %d\n", (int)oi_button_get(IX_OI_BUTTON(1, 5, OI_B_UP)));
+	printf("left, down = %d\n", (int)oi_button_get(IX_OI_BUTTON(1, 5, OI_B_DN)));
 	motor_set(IX_MOTOR(2), oi_arm);
-	printf("Arm: %d\n", (int)oi_arm);
 }
 
 void telop_spin(void) {
