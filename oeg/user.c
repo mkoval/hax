cac691028ee8a714a7a1518b4cfc546d901d0996
Michael Koval <koval.michael@gmail.com>
2011-01-25 21:13:06 -0500

    Latest version of OEG

--- oeg/user.c
+++ oeg/user.c
@@ -13,17 +13,18 @@ void auton_spin(void) {
 
 void telop_loop(void) {
 	int8_t oi_left  = oi_group_get(OI_JOY_L_Y(1));
-	int8_t oi_right = oi_group_get(OI_JOY_R_Y(1));
-	int8_t oi_arm   = oi_rocker_get(OI_ROCKER_L(1));
+	int8_t oi_right = oi_group_get(OI_JOY_R_X(1)); // bug
+	int8_t oi_arm   = oi_group_get(OI_ROCKER_L(1));
+	int8_t oi_suck  = oi_group_get(OI_ROCKER_R(1));
 
-	motor_set(MOTOR_DRIVE_L1, oi_left);
-	motor_set(MOTOR_DRIVE_L2, oi_left);
-	motor_set(MOTOR_DRIVE_R1, -oi_right);
-	motor_set(MOTOR_DRIVE_R2, -oi_right);
-	motor_set(MOTOR_ARM_L1, oi_arm);
-	motor_set(MOTOR_ARM_R2, oi_arm);
-	motor_set(MOTOR_ARM_R1, -oi_arm);
-	motor_set(MOTOR_ARM_R2, -oi_arm);
+	printf("left stick: %3d\n", oi_left);
+	motor_set(MOTOR_DRIVE_L, +oi_left);
+	motor_set(MOTOR_DRIVE_R, -oi_right);
+	motor_set(MOTOR_ARM_L1,  +oi_arm);
+	motor_set(MOTOR_ARM_R2,  +oi_arm);
+	motor_set(MOTOR_ARM_R1,  -oi_arm);
+	motor_set(MOTOR_ARM_R2,  -oi_arm);
+	motor_set(MOTOR_SUCKER,   oi_suck);
 }
 
 void telop_spin(void) {
