9b76ce2cb34f6ba913c53771735fd94cd3ece03d
Cody Schafer <cpschafer@gmail.com>
2011-01-29 18:40:48 -0500

    cortex: add calculations for bit setting.

--- arch_cortex/cortex.h
+++ arch_cortex/cortex.h
@@ -1,8 +1,23 @@
 #ifndef CORTEX_H_
 #define CORTEX_H_
-#include "vex_hw.h"
 
-extern spi_packet_t m2u;
-extern spi_packet_t u2m;
+#include <stm32f10x.h>
+#include <stdint.h>
+/** RCC */
+
+/* CIR */
+#define RCC_CIR_CSSF_B 7
+#define RCC_CIR_CSSC_B 23
+
+/* CR */
+#define RCC_CR_CSSON_B 19
+#define RCC_CR_HSEON_B 16
+
+
+//#define PERIPH_BB(reg, pin) (*(PERIPH_BB_BASE + (&(reg) - PERIPH_BASE) * 32 + (pin * 4)))
+
+#define PERIPH_BIT(system, regn, bitn) (*((uint32_t *)(PERIPH_BB_BASE + \
+			(((uintptr_t)&((system)->regn) - PERIPH_BASE) * 32) \
+			        + (system##_##regn##_##bitn##_B))))
 
 #endif
