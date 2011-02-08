9297ed042f0a2d4130990f04882c72a9da970dc1
Cody Schafer <cpschafer@gmail.com>
2011-02-07 20:12:02 -0500

    cortex: don't need those headers.

--- arch_cortex/hax.c
+++ arch_cortex/hax.c
@@ -6,24 +6,13 @@
 #include "stm32f10x.h"
 #include "vex_hw.h"
 
-#if defined(USE_STDPERIPH_DRIVER)
-#include "stm32f10x_adc.h"
-#include "stm32f10x_dma.h"
-#include "stm32f10x_rcc.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_usart.h"
-#include "stm32f10x_spi.h"
-#include "stm32f10x_tim.h"
-#include "stm32f10x_misc.h"
-#include "core_cm3.h"
-#endif
-
 #include "compilers.h"
 #include "init.h"
 #include "clocks.h"
 #include "usart.h"
 #include "spi.h"
 #include "exti.h"
+#include "motor.h"
 
 static spi_packet_t m2u;
 static spi_packet_t u2m;
