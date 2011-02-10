96a9bb6b988658449209ae74b4f717523172f033
Cody Schafer <cpschafer@gmail.com>
2011-02-10 02:42:44 -0500

    oeg: now a test for 2wm

--- oeg/user.c
+++ oeg/user.c
@@ -13,18 +13,8 @@ void auton_spin(void) {
 
 void telop_loop(void) {
 	int8_t oi_left  = oi_group_get(OI_JOY_L_Y(1));
-	int8_t oi_right = oi_group_get(OI_JOY_R_X(1)); // bug
-	int8_t oi_arm   = oi_group_get(OI_ROCKER_L(1));
-	int8_t oi_suck  = oi_group_get(OI_ROCKER_R(1));
 
-	printf("left stick: %3d\n", oi_left);
-	motor_set(MOTOR_DRIVE_L, +oi_left);
-	motor_set(MOTOR_DRIVE_R, -oi_right);
-	motor_set(MOTOR_ARM_L1,  +oi_arm);
-	motor_set(MOTOR_ARM_R2,  +oi_arm);
-	motor_set(MOTOR_ARM_R1,  -oi_arm);
-	motor_set(MOTOR_ARM_R2,  -oi_arm);
-	motor_set(MOTOR_SUCKER,   oi_suck);
+	motor_set(MOTOR_TEST, oi_left);
 }
 
 void telop_spin(void) {
--- oeg/user.h
+++ oeg/user.h
@@ -5,6 +5,7 @@
 
 #if defined(ARCH_CORTEX)
 enum {
+	MOTOR_TEST = IX_MOTOR(1),
 	MOTOR_DRIVE_R = IX_MOTOR(2),
 	MOTOR_DRIVE_L = IX_MOTOR(3),
 	MOTOR_ARM_R1  = IX_MOTOR(4),
@@ -21,7 +22,8 @@ enum {
 	MOTOR_ARM_R2  = IX_MOTOR(4),
 	MOTOR_ARM_L1  = IX_MOTOR(5),
 	MOTOR_ARM_L2  = IX_MOTOR(6),
-	MOTOR_SUCKER  = IX_MOTOR(7)
+	MOTOR_SUCKER  = IX_MOTOR(7),
+	MOTOR_TEST = IX_MOTOR(8)
 };
 #else
 # error "unsupported architecture"
